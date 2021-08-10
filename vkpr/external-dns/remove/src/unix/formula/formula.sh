af5c1dc3e2e06dad6712d4bf3216268c91aa6b04
HumbertoPeluso <humbertopeluso@yahoo.com.br>
2021-08-10 16:33:28 -0300

    remove function

---
+++ vkpr/external-dns/remove/src/unix/formula/formula.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+runFormula() {
+  echoColor "yellow" "Removendo external-dns..."
+  VKPR_HOME=~/.vkpr
+  rm -rf $VKPR_HOME/values/external-dns
+  helm delete vkpr
+}
+
+echoColor() {
+  case $1 in
+    red)
+      echo "$(printf '\033[31m')$2$(printf '\033[0m')"
+      ;;
+    green)
+      echo "$(printf '\033[32m')$2$(printf '\033[0m')"
+      ;;
+    yellow)
+      echo "$(printf '\033[33m')$2$(printf '\033[0m')"
+      ;;
+    blue)
+      echo "$(printf '\033[34m')$2$(printf '\033[0m')"
+      ;;
+    cyan)
+      echo "$(printf '\033[36m')$2$(printf '\033[0m')"
+      ;;
+    esac
+}
