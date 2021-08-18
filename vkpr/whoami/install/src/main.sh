b41ad1e37c9e66a01f241387991762aade6d8dbb
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-08-18 14:57:56 -0300

    Added the use of global variables

--- vkpr/whoami/install/src/main.sh
+++ vkpr/whoami/install/src/main.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ ! -d $CURRENT_PWD/global ]; then
+  echo "Doesn't initializated the vkpr... Call again the function"
+  rit vkpr init
+  exit;
+fi
+
+source $CURRENT_PWD/global/log.sh
+source $CURRENT_PWD/global/var.sh
+source $CURRENT_PWD/global/helper.sh
+source $CURRENT_PWD/global/.env
+
 # shellcheck source=/dev/null
 . "$(dirname "$0")"/unix/formula/formula.sh --source-only
 
