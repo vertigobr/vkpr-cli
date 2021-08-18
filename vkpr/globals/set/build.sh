21581761b081ea7f932019020a7f6b089fb254fe
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-08-18 14:54:41 -0300

    Add formula to set default globals

---
+++ vkpr/globals/set/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BIN_FOLDER=bin
+BINARY_NAME_UNIX=run.sh
+ENTRY_POINT_UNIX=main.sh
+
+#bash-build:
+	mkdir -p $BIN_FOLDER
+	cp -r src/* $BIN_FOLDER
+	mv $BIN_FOLDER/$ENTRY_POINT_UNIX $BIN_FOLDER/$BINARY_NAME_UNIX
+	chmod +x $BIN_FOLDER/$BINARY_NAME_UNIX
+
