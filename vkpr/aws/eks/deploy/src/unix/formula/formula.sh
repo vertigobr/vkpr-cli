0feb1b90ae768527ef50c2d25c824ced2ae2f0e1
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-09-02 13:22:46 -0300

    Deleted unused variable

--- vkpr/aws/eks/deploy/src/unix/formula/formula.sh
+++ vkpr/aws/eks/deploy/src/unix/formula/formula.sh
@@ -2,7 +2,6 @@
 
 runFormula() {
   PROJECT_ID=$(curl https://gitlab.com/api/v4/users/$GITLAB_USERNAME/projects | jq '.[0] | .id')
-  PIPELINE_ID=$(curl https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines | jq '.[0] | .id')
   DEPLOY_ID=$(curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs | jq '.[1] | .id')
   curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST -s https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs/$DEPLOY_ID/play > /dev/null
 }
--- vkpr/aws/eks/destroy/src/unix/formula/formula.sh
+++ vkpr/aws/eks/destroy/src/unix/formula/formula.sh
@@ -2,7 +2,6 @@
 
 runFormula() {
   PROJECT_ID=$(curl https://gitlab.com/api/v4/users/$GITLAB_USERNAME/projects | jq '.[0] | .id')
-  PIPELINE_ID=$(curl https://gitlab.com/api/v4/projects/$PROJECT_ID/pipelines | jq '.[0] | .id')
   DESTROY_ID=$(curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs | jq '.[0] | .id')
   curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST -s https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs/$DESTROY_ID/play > /dev/null
 }
