0b03acd2140b37eb7c01cc652e1b64b9279d2194
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-09-10 15:30:20 -0300

    Infra refactored code and formula documentation update

--- vkpr/infra/up/src/unix/formula/formula.sh
+++ vkpr/infra/up/src/unix/formula/formula.sh
@@ -11,6 +11,8 @@ runFormula() {
   echoColor "yellow" "Volumes used:"
   echo "Two local unamed docker volumes"
 
+  VKPR_CONFIG_REGISTRY=$VKPR_HOME/config/registry.yaml
+
   # TODO: test dependencies
 
   # VKPR home is "~/.vkpr"
@@ -24,7 +26,7 @@ runFormula() {
 configRegistry() {
   #HOST_IP="172.17.0.1" # linux native
   HOST_IP="host.k3d.internal"
-  cat > $VKPR_HOME/config/registry.yaml << EOF
+  cat > $VKPR_CONFIG_REGISTRY << EOF
 mirrors:
   "docker.io":
     endpoint:
@@ -34,31 +36,31 @@ EOF
 
 startCluster() {
   # local registry
-  if ! $(k3d cluster list | grep -q "vkpr-local"); then
-    k3d cluster create vkpr-local \
+  if ! $($VKPR_K3D cluster list | grep -q "vkpr-local"); then
+    $VKPR_K3D cluster create vkpr-local \
       -p "8000:80@loadbalancer" \
       -p "8001:443@loadbalancer" \
       --k3s-server-arg '--no-deploy=traefik' \
       --registry-use k3d-registry.localhost \
-      --registry-config $VKPR_HOME/config/registry.yaml
+      --registry-config $VKPR_CONFIG_REGISTRY
   else
     echoColor "yellow" "Cluster vkpr-local already started, skipping."
   fi
   # use cluster
-  $VKPR_HOME/bin/kubectl config use-context k3d-vkpr-local
-  $VKPR_HOME/bin/kubectl cluster-info
+  $VKPR_KUBECTL config use-context k3d-vkpr-local
+  $VKPR_KUBECTL cluster-info
 }
 
 startRegistry() {
   # local registry
-  if ! $(k3d registry list | grep -q "k3d-registry\.localhost"); then
-    k3d registry create registry.localhost -p 5000
+  if ! $($VKPR_K3D registry list | grep -q "k3d-registry\.localhost"); then
+    $VKPR_K3D registry create registry.localhost -p 5000
   else
     echoColor "yellow" "Registry already started, skipping."
   fi
   # docker hub mirror
-  if ! $(k3d registry list | grep -q "k3d-mirror\.localhost"); then
-    k3d registry create mirror.localhost -i vertigo/registry-mirror -p 5001
+  if ! $($VKPR_K3D registry list | grep -q "k3d-mirror\.localhost"); then
+    $VKPR_K3D registry create mirror.localhost -i vertigo/registry-mirror -p 5001
   else
     echoColor "yellow" "Mirror already started, skipping."
   fi
