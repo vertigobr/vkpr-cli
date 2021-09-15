7d123c706247566a192830022032e776b00f21a3
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-09-15 14:55:32 -0300

    Change global values insert

--- vkpr/keycloak/install/src/unix/formula/formula.sh
+++ vkpr/keycloak/install/src/unix/formula/formula.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 
 runFormula() {
-  PG_USER="postgres"
-  PG_PASSWORD=$($VKPR_JQ -r '.credential.password' ~/.rit/credentials/default/postgres)
-  PG_DATABASE_NAME="keycloak"
-  VKPR_KEYCLOAK_YAML=$(dirname "$0")/utils/keycloak.yaml
-
-  checkGlobalConfig $DOMAIN "keycloak.localhost" "keycloak.domain" "KEYCLOAK_DOMAIN"
-  checkGlobalConfig $SECURE "false" "keycloak.secure" "KEYCLOAK_SECURE" # Unused Variable TODO: See if is secure and then enable the cert-manager and TLS
+  checkGlobalConfig $DOMAIN "localhost" "domain" "DOMAIN"
+  checkGlobalConfig $SECURE "false" "secure" "SECURE"
   checkGlobalConfig $ADMIN_USER "admin" "keycloak.admin_user" "KEYCLOAK_ADMIN_USER"
   checkGlobalConfig $ADMIN_PASSWORD "vkpr123" "keycloak.admin_password" "KEYCLOAK_ADMIN_PASSWORD"
+
+  local PG_USER="postgres"
+  local PG_PASSWORD=$($VKPR_JQ -r '.credential.password' ~/.rit/credentials/default/postgres)
+  local PG_DATABASE_NAME="keycloak"
+  local VKPR_ENV_KEYCLOAK_DOMAIN="keycloak.${VKPR_ENV_DOMAIN}"
 
   addRepoKeycloak
   installKeycloak
 }
 
 addRepoKeycloak(){
-  $VKPR_HELM repo add bitnami https://charts.bitnami.com/bitnami
-  $VKPR_HELM repo update
+  $VKPR_HELM repo add bitnami https://charts.bitnami.com/bitnami --force-update
 }
 
 verifyExistingPostgres(){
-  EXISTING_POSTGRES=$($VKPR_KUBECTL get po/vkpr-postgres-postgresql-0 -o name --ignore-not-found | cut -d "/" -f2)
+  local EXISTING_POSTGRES=$($VKPR_KUBECTL get pod/vkpr-postgres-postgresql-0 -o name --ignore-not-found | cut -d "/" -f2)
   if [[ $EXISTING_POSTGRES = "vkpr-postgres-postgresql-0" ]]; then
-    POSTGRES=$($VKPR_KUBECTL wait --for=condition=Ready po/vkpr-postgres-postgresql-0 -o name | cut -d "/" -f2)
+    local POSTGRES=$($VKPR_KUBECTL wait --for=condition=Ready pod/vkpr-postgres-postgresql-0 -o name | cut -d "/" -f2)
     if [[ $POSTGRES = "vkpr-postgres-postgresql-0" ]]; then
       echo "true"
       return
@@ -33,32 +32,43 @@ verifyExistingPostgres(){
 }
 
 settingKeycloak(){
-  $VKPR_YQ eval '.ingress.hosts[0].host = "'$VKPR_ENV_KEYCLOAK_DOMAIN'" | .ingress.tls[0].hosts[0] = "'$VKPR_ENV_KEYCLOAK_DOMAIN'" |
-  .auth.adminUser = "'$VKPR_ENV_KEYCLOAK_ADMIN_USER'" |
-  .auth.adminPassword = "'$VKPR_ENV_KEYCLOAK_ADMIN_PASSWORD'"' "$VKPR_KEYCLOAK_YAML" \
- | $VKPR_HELM upgrade -i -f - vkpr-keycloak bitnami/keycloak
-}
-
-settingKeycloakDB(){
-  $VKPR_YQ eval '.ingress.hosts[0].host = "'$VKPR_ENV_KEYCLOAK_DOMAIN'" | .ingress.tls[0].hosts[0] = "'$VKPR_ENV_KEYCLOAK_DOMAIN'" |
-  .externalDatabase.host = "postgres-postgresql" | .externalDatabase.port = "5432" |
-  .externalDatabase.user = "'$PG_USER'" | .externalDatabase.password = "'$PG_PASSWORD'" |
-  .externalDatabase.database = "'$PG_DATABASE_NAME'" |
-  .auth.adminUser = "'$VKPR_ENV_KEYCLOAK_ADMIN_USER'" | .auth.adminPassword = "'$VKPR_ENV_KEYCLOAK_ADMIN_PASSWORD'"' "$VKPR_KEYCLOAK_YAML" \
- | $VKPR_HELM upgrade -i -f - vkpr-keycloak bitnami/keycloak
+  local DB_EXIST=$1
+  local YQ_VALUES='.ingress.hostname = "'$VKPR_ENV_KEYCLOAK_DOMAIN'" |
+    .auth.adminUser = "'$VKPR_ENV_KEYCLOAK_ADMIN_USER'" |
+    .auth.adminPassword = "'$VKPR_ENV_KEYCLOAK_ADMIN_PASSWORD'"
+  '
+  if [[ $VKPR_ENV_SECURE == true ]]; then
+    YQ_VALUES=''$YQ_VALUES' |
+      .ingress.certManager = true |
+      .ingress.tls = true
+    '
+  fi
+  if [ $DB_EXIST = true ]; then
+    YQ_VALUES=''$YQ_VALUES' |
+      .postgresql.enabled = false |
+      .externalDatabase.host = "vkpr-postgres-postgresql" |
+      .externalDatabase.port = "5432" |
+      .externalDatabase.user = "'$PG_USER'" |
+      .externalDatabase.password = "'$PG_PASSWORD'" |
+      .externalDatabase.database = "'$PG_DATABASE_NAME'"
+    '
+  fi
+  $VKPR_YQ eval "$YQ_VALUES" "$VKPR_KEYCLOAK_VALUES" \
+  | $VKPR_HELM upgrade -i -f - vkpr-keycloak bitnami/keycloak
 }
 
 installKeycloak(){
+  local VKPR_KEYCLOAK_VALUES=$(dirname "$0")/utils/keycloak.yaml
   if [[ $(verifyExistingPostgres) = "true" ]]; then
     echoColor "yellow" "Initializing Keycloak with Postgres already created"
-    VKPR_KEYCLOAK_YAML=$(dirname "$0")/utils/keycloak-db.yaml
-    PG_EXISTING_DATABASE=$(checkExistingDatabase $PG_USER $PG_PASSWORD $PG_DATABASE_NAME $PG_DATABASE_NAME)
-    if [[ ! -n $PG_EXISTING_DATABASE ]]; then
+    local PG_EXISTING_DATABASE=$(checkExistingDatabase $PG_USER $PG_PASSWORD $PG_DATABASE_NAME $PG_DATABASE_NAME)
+    if [ $PG_EXISTING_DATABASE != "keycloak" ]; then
+      echoColor "yellow" "Creating Database Instance..."
       createDatabase $PG_USER $PG_PASSWORD $PG_DATABASE_NAME
     fi
-    settingKeycloakDB
+    settingKeycloak true
   else
     echoColor "yellow" "There is no Postgres installed, Keycloak will generate one for your use"
-    settingKeycloak
+    settingKeycloak false
   fi
 }
