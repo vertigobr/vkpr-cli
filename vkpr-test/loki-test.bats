1f17697783aa6828a6a91e42f31be7f8ceab5cbb
jpeedroza <pedrozajoaopedro@gmail.com>
2021-10-11 09:24:18 -0300

    added loki versioning and use of globals

--- vkpr-test/loki-test.bats
+++ vkpr-test/loki-test.bats
@@ -1,4 +1,5 @@
 VKPR_HOME=~/.vkpr
+
 setup_file() {
     load 'common-setup'
     _common_setup
@@ -7,7 +8,6 @@ setup_file() {
     else
         echo "setup: installing loki...." >&3
         rit vkpr loki install
-        $VKPR_HOME/bin/kubectl wait --for=condition=ready --timeout=1m pod --all
         sleep 2
     fi
 }
@@ -18,13 +18,22 @@ setup() {
 }
 
 @test "curl to Loki must return ready" {
-    run echo "$($VKPR_HOME/bin/kubectl run --wait --timeout 1m curl-test --image=radial/busyboxplus:curl -i --rm --restart=Never --command -- curl -s  -H "Content-Type: application/json" http://vkpr-loki-stack:3100/ready)"
+    run echo "$($VKPR_HOME/bin/kubectl run --namespace vkpr --wait --rm -it --restart=Never --image=curlimages/curl curl --command -- curl -sH "Content-Type: application/json" http://loki-stack:3100/ready)"
     actual="${lines[0]}"
     trim "$actual"
     actual="$TRIMMED"
     expected="ready"
     assert_equal "$actual" "$expected"
-    
+}
+
+teardown_file() {
+    if [ "$VKPR_TEST_SKIP_TEARDOWN" == "true" ]; then
+        echo "teardown: skipping teardown due to VKPR_TEST_SKIP_TEARDOWN=true" >&3
+    else
+        echo "teardown: uninstalling loki..." >&3
+        rit vkpr loki remove
+    fi
+    _common_teardown
 }
 
 trim() {
--- vkpr/loki/install/src/unix/formula/formula.sh
+++ vkpr/loki/install/src/unix/formula/formula.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 runFormula() {
-  VKPR_EXTERNAL_LOKI_VALUES=$(dirname "$0")/utils/loki.yaml
+  local VKPR_LOKI_VALUES=$(dirname "$0")/utils/loki.yaml
+
+  checkGlobalConfig $DOMAIN "localhost" "domain" "DOMAIN"
+  checkGlobal "loki.resources" $VKPR_LOKI_VALUES "resources"
+  checkGlobal "loki.extraEnv" $VKPR_LOKI_VALUES
 
   addRepLoki
   installLoki
@@ -14,27 +18,39 @@ addRepLoki(){
 }
 
 installLoki(){
-  $VKPR_HELM upgrade --install \
-    --namespace $VKPR_K8S_NAMESPACE --create-namespace \
-    --wait --timeout 5m \
-    loki-stack -f $VKPR_EXTERNAL_LOKI_VALUES grafana/loki-stack
+  $VKPR_YQ eval "$YQ_VALUES" "$VKPR_LOKI_VALUES" \
+  | $VKPR_HELM upgrade -i --version "$VKPR_LOKI_VERSION" \
+    --create-namespace --namespace $VKPR_K8S_NAMESPACE\
+    --wait -f - loki-stack grafana/loki-stack
 }
 
 existGrafana() {
-  if [[ $(checkExistingGrafana) = "vkpr-prometheus-stack-grafana" ]]; then
-    local LOGINGRAFANA="$($VKPR_KUBECTL get secret --namespace $VKPR_K8S_NAMESPACE vkpr-prometheus-stack-grafana -o yaml | $VKPR_YQ eval '.data.admin-user' - | base64 -d):$($VKPR_KUBECTL get secret --namespace $VKPR_K8S_NAMESPACE vkpr-prometheus-stack-grafana -o yaml | $VKPR_YQ eval '.data.admin-password' - | base64 -d)"
-    local TOKEN_API_GRAFANA=$(curl -k -X POST -H "Host: grafana.localhost" -H "Content-Type: application/json" -d '{"name": "apikeycurl","role": "Admin"}' http://$LOGINGRAFANA@127.0.0.1:8000/api/auth/keys | $VKPR_JQ --raw-output '.key')
+  if [[ $(checkPodName "prometheus-stack-grafana") = "true" ]]; then
+    local LOGINGRAFANA="$($VKPR_KUBECTL get secret --namespace $VKPR_K8S_NAMESPACE prometheus-stack-grafana -o yaml \
+                          | $VKPR_YQ eval '.data.admin-user' - \
+                          | base64 -d):$($VKPR_KUBECTL get secret --namespace $VKPR_K8S_NAMESPACE prometheus-stack-grafana -o yaml \
+                          | $VKPR_YQ eval '.data.admin-password' - | base64 -d)"
+
+    local TOKEN_API_GRAFANA=$(curl -sk -X POST \
+                              -H "Host: grafana.$VKPR_ENV_DOMAIN" -H "Content-Type: application/json" \
+                              -d '{"name": "apikeycurl","role": "Admin"}' \
+                              http://$LOGINGRAFANA@127.0.0.1:8000/api/auth/keys | $VKPR_JQ --raw-output '.key')
+
     if [[ $TOKEN_API_GRAFANA == "" ]]; then
       echoColor "red" "Api Token can only be request once or ingress is not installed."
     fi
-    curl -K -X -H "Host: grafana.localhost" -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN_API_GRAFANA" -d '
-      {
-        "name":"loki",
-        "type":"loki",
-        "url":"vkpr-loki-stack.'$VKPR_K8S_NAMESPACE'.svs.cluster.local:3100",
-        "access":"proxy",
-        "basicAuth":false,
-        "editable": true
-      }' http://127.0.0.1:8000/api/datasources
+
+    curl -sK -X \
+    -H "Host: grafana.localhost" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $TOKEN_API_GRAFANA" \
+    -d '{
+          "name":"loki",
+          "type":"loki",
+          "url":"loki-stack.'$VKPR_K8S_NAMESPACE'.svs.cluster.local:3100",
+          "access":"proxy",
+          "basicAuth":false,
+          "editable": true
+        }' http://127.0.0.1:8000/api/datasources
   fi
 }
