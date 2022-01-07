71e8336db8275da4785a9c52b387f31beb7d3501
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-01-07 09:38:06 -0300

    create formula to import grafana dashboards

---
+++ vkpr/prometheus-stack/import/src/unix/formula/formula.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+runFormula() {
+  echoColor "bold" "$(echoColor "green" "Importing Dashboard...")"
+  local JSON_FILE=$(cat $DASHBOARD_PATH)
+  dashboard="$DASHBOARD_NAME-grafana" $VKPR_YQ eval '(.metadata.name, .metadata.labels.app) = strenv(dashboard)' $(dirname "$0")/utils/dashboard.yaml > $DASHBOARD_NAME.yaml
+  $VKPR_YQ eval -i '.metadata.namespace = "'$VKPR_K8S_NAMESPACE'"' $DASHBOARD_NAME.yaml
+  $VKPR_YQ eval '.data.["'$DASHBOARD_NAME.json'"] = ('"$JSON_FILE"' | to_json)' $DASHBOARD_NAME.yaml \
+  | $VKPR_KUBECTL apply -f -
+  rm $DASHBOARD_NAME.yaml
+}
