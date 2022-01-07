78d254311a0aa08461eb1c79fab5d6d28ff8dd83
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-01-07 09:39:26 -0300

    add merge helper into prometheus-stack formula and new options to vkpr.yaml

--- vkpr/prometheus-stack/remove/src/unix/formula/formula.sh
+++ vkpr/prometheus-stack/remove/src/unix/formula/formula.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 runFormula() {
+  echoColor "bold" "$(echoColor "green" "Removing Prometheus-stack...")"
   $VKPR_HELM uninstall --namespace $VKPR_K8S_NAMESPACE prometheus-stack
+  $VKPR_KUBECTL delete cm -n vkpr -l grafana_dashboard=true > /dev/null
 }
