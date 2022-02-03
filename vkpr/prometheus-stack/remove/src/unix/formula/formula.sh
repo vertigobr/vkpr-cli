d61d0d89ade858d0797a6d884040b02e7af53580
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-03 10:35:59 -0300

    fix namespace variables

--- vkpr/prometheus-stack/remove/src/unix/formula/formula.sh
+++ vkpr/prometheus-stack/remove/src/unix/formula/formula.sh
@@ -3,5 +3,5 @@
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing Prometheus-stack...")"
   $VKPR_HELM uninstall --namespace $VKPR_K8S_NAMESPACE prometheus-stack
-  $VKPR_KUBECTL delete cm -n vkpr -l grafana_dashboard=true > /dev/null
+  $VKPR_KUBECTL delete cm -n $VKPR_K8S_NAMESPACE -l grafana_dashboard=true > /dev/null
 }
