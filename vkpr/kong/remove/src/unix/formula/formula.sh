810d79930beaf4ed00800b55637509497c24a82c
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 15:37:09 -0300

    style(kong): lint the code and fix errors

--- vkpr/kong/remove/src/unix/formula/formula.sh
+++ vkpr/kong/remove/src/unix/formula/formula.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
+  $VKPR_KUBECTL get namespace | grep -q kong && uninstallKongDP
+  uninstallKong
+}
+
+uninstallKong() {
   echoColor "bold" "$(echoColor "green" "Removing Kong...")"
 
-  KONG_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=kong,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall kong -n $KONG_NAMESPACE
-  $VKPR_KUBECTL delete secret -A -l app.kubernetes.io/instance=kong,vkpr=true 2> /dev/null
+  KONG_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=kong,vkpr=true -o=yaml |\
+                   $VKPR_YQ e ".items[].metadata.namespace" - |\
+                   head -n1)
 
-  $VKPR_HELM uninstall kong-dp -n kong 2> /dev/null
-  $VKPR_KUBECTL delete ns kong 2> /dev/null
+  $VKPR_HELM uninstall --namespace "$KONG_NAMESPACE" kong 2> /dev/null || echoColor "red" "VKPR Kong not found"
+  $VKPR_KUBECTL delete secret -A --ignore-not-found=true -l app.kubernetes.io/instance=kong,vkpr=true > /dev/null
+}
+
+uninstallKongDP() {
+  echoColor "bold" "$(echoColor "green" "Removing Kong DP...")"
+
+  $VKPR_HELM uninstall kong-dp -n kong 2> /dev/null || echoColor "red" "VKPR Kong not found"
+  $VKPR_KUBECTL delete ns kong 2> /dev/null || echoColor "red" "VKPR Kong not found"
 }
