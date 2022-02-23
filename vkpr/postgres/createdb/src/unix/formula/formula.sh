8af3adc343ed62d35dc98ab8e819b76b3599f43a
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 15:41:34 -0300

    style(postgres): lint the code

--- vkpr/postgres/createdb/src/unix/formula/formula.sh
+++ vkpr/postgres/createdb/src/unix/formula/formula.sh
@@ -2,16 +2,19 @@
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Creating database $DBNAME in postgresql...")"
+  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "postgresql.namespace" "POSTGRESQL_NAMESPACE"
 
-  local PG_PASSWORD=$($VKPR_JQ -r '.credential.password' ~/.rit/credentials/default/postgres)
-  local PG_HOST="postgres-postgresql"
+  local PG_PASSWORD; PG_PASSWORD=$($VKPR_JQ -r '.credential.password' ~/.rit/credentials/default/postgres)
+  local PG_HOST; PG_HOST="postgres-postgresql"
 
-  checkGlobalConfig $VKPR_K8S_NAMESPACE "vkpr" "postgresql.namespace" "NAMESPACE"
+  if [[ $(checkPodName "$VKPR_ENV_POSTGRESQL_NAMESPACE" "postgres-postgresql") == "true" ]]; then
+    $VKPR_KUBECTL get pod -n "$VKPR_ENV_POSTGRESQL_NAMESPACE" | grep -q pgpool && PG_HOST="postgres-postgresql-pgpool"
 
-  if [[ $(checkPodName $VKPR_ENV_NAMESPACE "postgres-postgresql") = "true" ]]; then
-    [[ ! -z $($VKPR_KUBECTL get pod -n $VKPR_ENV_NAMESPACE | grep pgpool) ]] && PG_HOST="postgres-postgresql-pgpool"
-    $VKPR_KUBECTL run init-db --rm -it --restart="Never" --namespace $VKPR_ENV_NAMESPACE --image docker.io/bitnami/postgresql-repmgr:11.14.0-debian-10-r12 --env="PGUSER=postgres" --env="PGPASSWORD=$PG_PASSWORD" --env="PGHOST=${PG_HOST}" --env="PGPORT=5432" --env="PGDATABASE=postgres" \
-    --command -- psql -c '\x' -c "CREATE USER $DBUSER WITH ENCRYPTED PASSWORD '$DBPASSWORD';" -c "CREATE DATABASE $DBNAME;" -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER"
+    $VKPR_KUBECTL run init-db --rm -it --restart="Never" --namespace "$VKPR_ENV_POSTGRESQL_NAMESPACE" \
+      --image docker.io/bitnami/postgresql-repmgr:11.14.0-debian-10-r12 \
+      --env="PGUSER=postgres" --env="PGPASSWORD=$PG_PASSWORD" --env="PGHOST=${PG_HOST}" --env="PGPORT=5432" --env="PGDATABASE=postgres" \
+      --command -- psql -c '\x' -c "CREATE USER $DBUSER WITH ENCRYPTED PASSWORD '$DBPASSWORD';" \
+                    -c "CREATE DATABASE $DBNAME;" -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER"
     else
     echoColor "red" "Error, Postgresql doesn't up or installed yet"
   fi
