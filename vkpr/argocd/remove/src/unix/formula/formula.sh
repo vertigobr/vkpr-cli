e311b74fbd8bb33e5505b6ffca3b7ee08b46c8fb
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 13:53:46 -0300

    style(argocd): lint the code and fix errors

--- vkpr/argocd/remove/src/unix/formula/formula.sh
+++ vkpr/argocd/remove/src/unix/formula/formula.sh
@@ -3,12 +3,28 @@
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing ArgoCD...")"
 
-  ARGOCD_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=argocd,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall argocd -n $ARGOCD_NAMESPACE 2> /dev/null
+  uninstallArgo
+  $VKPR_KUBECTL get crd | grep -q applicationsets.argoproj.io && uninstallApplicationset
+  uninstallResources
+}
 
-  ARGOCD_APPLICATIONSET_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=argocd-applicationset,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall argocd-applicationset -n $ARGOCD_APPLICATIONSET_NAMESPACE 2> /dev/null
+uninstallArgo() {
+  ARGOCD_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=argocd,vkpr=true -o=yaml |\
+                     $VKPR_YQ e ".items[].metadata.namespace" - |\
+                     head -n1)
 
-  $VKPR_KUBECTL delete secret -A -l argocd.argoproj.io/secret-type=repository,vkpr=true > /dev/null
-  $VKPR_KUBECTL delete ApplicationSet -A -l vkpr=true > /dev/null
+  $VKPR_HELM uninstall argocd -n "$ARGOCD_NAMESPACE" 2> /dev/null || echoColor "red" "VKPR Argocd not found"
+}
+
+uninstallApplicationset() {
+  ARGOCD_APPLICATIONSET_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=argocd-applicationset,vkpr=true -o=yaml |\
+                                    $VKPR_YQ e ".items[].metadata.namespace" - |\
+                                    head -n1)
+
+  $VKPR_HELM uninstall argocd-applicationset -n "$ARGOCD_APPLICATIONSET_NAMESPACE" 2> /dev/null || echoColor "red" "VKPR Argocd Applicationset not found"
+}
+
+uninstallResources() {
+  $VKPR_KUBECTL delete secret -A --ignore-not-found=true -l argocd.argoproj.io/secret-type=repository,vkpr=true > /dev/null
+  $VKPR_KUBECTL delete ApplicationSet -A --ignore-not-found=true -l vkpr=true > /dev/null
 }
--- vkpr/argocd/set/applicationset/src/unix/formula/formula.sh
+++ vkpr/argocd/set/applicationset/src/unix/formula/formula.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 runFormula() {
-  local VKPR_APPLICATIONSET_VALUES=$(dirname "$0")/utils/applicationset.yaml
-  local REPO_NAME=$(echo $REPO_URL | awk -F "/" '{ print $NF }' | cut -d "." -f1)
+  checkGlobalConfig "argocd" "argocd" "argocd.namespace" "ARGOCD_NAMESPACE"
 
-  checkGlobalConfig "argocd" "argocd" "argocd.namespace" "NAMESPACE"
+  local VKPR_APPLICATIONSET_VALUES REPO_NAME; 
+  VKPR_APPLICATIONSET_VALUES=$(dirname "$0")/utils/applicationset.yaml
+  REPO_NAME=$(echo "$REPO_URL" | awk -F "/" '{ print $NF }' | cut -d "." -f1)
 
-  if [[ -z $($VKPR_KUBECTL get deployment argocd-application-controller -n $VKPR_ENV_NAMESPACE --ignore-not-found | awk 'NR>1{print $1}') ]]; then
+  if [[ -z $($VKPR_KUBECTL get deployment argocd-application-controller -n "$VKPR_ENV_ARGOCD_NAMESPACE" --ignore-not-found | awk 'NR>1{print $1}') ]]; then
     echoColor "red" "Dont have Addon applicationset installed in this cluster"
   else
     echoColor "green" "Creating Applicationset in Argocd"
-    $VKPR_YQ eval ' .metadata.name = "'$REPO_NAME-applicationset'" |
-      .metadata.namespace = "'$VKPR_ENV_NAMESPACE'" |
-      .spec.generators[0].git.repoURL = "'$REPO_URL'" |
-      .spec.template.spec.source.repoURL = "'$REPO_URL'"
-    ' $VKPR_APPLICATIONSET_VALUES | $VKPR_KUBECTL apply -f -
+    $VKPR_YQ eval " .metadata.name = \"$REPO_NAME-applicationset\" |
+      .metadata.namespace = \"$VKPR_ENV_ARGOCD_NAMESPACE\" |
+      .spec.generators[0].git.repoURL = \"$REPO_URL\" |
+      .spec.template.spec.source.repoURL = \"$REPO_URL\"
+    " "$VKPR_APPLICATIONSET_VALUES" | $VKPR_KUBECTL apply -f -
   fi
 }
