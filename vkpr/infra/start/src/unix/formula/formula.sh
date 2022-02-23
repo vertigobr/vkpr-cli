6bfe055384e004cc6542af4d7f72c9532bb34cbf
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 14:32:04 -0300

    style(infra): lint the code

--- vkpr/infra/start/src/unix/formula/formula.sh
+++ vkpr/infra/start/src/unix/formula/formula.sh
@@ -1,14 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
-  local SERVERS=1
-  local AGENTS=1
-
-  checkGlobalConfig $HTTP_PORT "8000" "infra.httpPort" "HTTP_PORT"
-  checkGlobalConfig $HTTPS_PORT "8001" "infra.httpsPort" "HTTPS_PORT"
-  checkGlobalConfig $ENABLE_TRAEFIK "false" "infra.traefik.enabled" "TRAEFIK"
-  checkGlobalConfig $SERVERS "1" "infra.resources.masters" "K3D_SERVERS"
-  checkGlobalConfig $AGENTS "1" "infra.resources.workers" "K3D_AGENTS"
+  checkGlobalConfig "$HTTP_PORT" "8000" "infra.httpPort" "HTTP_PORT"
+  checkGlobalConfig "$HTTPS_PORT" "8001" "infra.httpsPort" "HTTPS_PORT"
+  checkGlobalConfig "$ENABLE_TRAEFIK" "false" "infra.traefik.enabled" "TRAEFIK"
+  checkGlobalConfig "1" "1" "infra.resources.masters" "K3D_SERVERS"
+  checkGlobalConfig "1" "1" "infra.resources.workers" "K3D_AGENTS"
 
   startInfos
   configRegistry
@@ -31,7 +28,7 @@ startInfos() {
 configRegistry() {
   #local HOST_IP="172.17.0.1"  #Linux Native
   local HOST_IP="host.k3d.internal"
-  cat > ${VKPR_CONFIG}/registry.yaml << EOF
+  cat > "${VKPR_CONFIG}"/registry.yaml << EOF
 mirrors:
   "docker.io":
     endpoint:
@@ -41,12 +38,13 @@ EOF
 
 # Create the local registry and Docker Hub Mirror
 startRegistry() {
-  if ! $(${VKPR_K3D} registry list | grep -q "k3d-registry\.localhost"); then
+  if ! ${VKPR_K3D} registry list | grep -q "k3d-registry\.localhost"; then
     ${VKPR_K3D} registry create registry.localhost -p 6000
   else
     echoColor "yellow" "Registry already started, skipping..."
   fi
-  if ! $(${VKPR_K3D} registry list | grep -q "k3d-mirror\.localhost"); then
+
+  if ! ${VKPR_K3D} registry list | grep -q "k3d-mirror\.localhost"; then
     ${VKPR_K3D} registry create mirror.localhost -i vertigo/registry-mirror -p 6001
   else
     echoColor "yellow" "Mirror already started, skipping..."
@@ -56,21 +54,24 @@ startRegistry() {
 # Starts K8S using Registries
 startCluster() {
   local TRAEFIK_FLAG=""
-  if [ "${ENABLE_TRAEFIK}" == "false" ]; then
+
+  if [ "${ENABLE_TRAEFIK}" == false ]; then
     TRAEFIK_FLAG="--disable=traefik@server:0"
   fi
-  if ! $(${VKPR_K3D} cluster list | grep -q "vkpr-local"); then
+
+  if ! ${VKPR_K3D} cluster list | grep -q "vkpr-local"; then
     ${VKPR_K3D} cluster create vkpr-local \
-      -s ${VKPR_ENV_K3D_SERVERS} -a ${VKPR_ENV_K3D_AGENTS} \
+      -s "${VKPR_ENV_K3D_SERVERS}" -a "${VKPR_ENV_K3D_AGENTS}" \
       -p "${VKPR_ENV_HTTP_PORT}:80@loadbalancer" \
       -p "${VKPR_ENV_HTTPS_PORT}:443@loadbalancer" \
       --k3s-arg "${TRAEFIK_FLAG}" \
       --registry-use k3d-registry.localhost \
-      --registry-config ${VKPR_CONFIG}/registry.yaml
+      --registry-config "${VKPR_CONFIG}"/registry.yaml
   ${VKPR_KUBECTL} config use-context k3d-vkpr-local
   else
     echoColor "bold" "$(echoColor "red" "Cluster vkpr-local already created.")"
   fi
+
   ${VKPR_KUBECTL} cluster-info
 }
 
--- vkpr/infra/up/src/unix/formula/formula.sh
+++ vkpr/infra/up/src/unix/formula/formula.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
-  [[ -f $CURRENT_PWD/vkpr.yaml ]] && cp $CURRENT_PWD/vkpr.yaml "$(dirname "$0")"
+  [[ -f $CURRENT_PWD/vkpr.yaml ]] && cp "$CURRENT_PWD"/vkpr.yaml "$(dirname "$0")"
   rit vkpr infra start --default
 }
 
