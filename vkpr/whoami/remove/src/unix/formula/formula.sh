84a595b0d014cddb4af208589cd1cb2e36c60a82
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 14:45:41 -0300

    style(whoami): lint the code

--- vkpr/whoami/remove/src/unix/formula/formula.sh
+++ vkpr/whoami/remove/src/unix/formula/formula.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing whoami...")"
 
-  WHOAMI_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=whoami,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall whoami -n $WHOAMI_NAMESPACE
+  WHOAMI_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=whoami,vkpr=true -o=yaml |\
+                     $VKPR_YQ e ".items[].metadata.namespace" - |\
+                     head -n1)
+
+  $VKPR_HELM uninstall whoami -n "$WHOAMI_NAMESPACE" 2> /dev/null || echoColor "red" "VKPR whoami not found"
 }
