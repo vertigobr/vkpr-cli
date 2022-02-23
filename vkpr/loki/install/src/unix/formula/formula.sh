7f9a777af4308a4fe0969fb238e30b73f560285e
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 14:56:22 -0300

    style(loki): lint the code

--- vkpr/loki/install/src/unix/formula/formula.sh
+++ vkpr/loki/install/src/unix/formula/formula.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
-  local VKPR_LOKI_VALUES=$(dirname "$0")/utils/loki.yaml
-
-  checkGlobalConfig "false" "false" "loki.metrics" "METRICS"
-  checkGlobalConfig $VKPR_K8S_NAMESPACE "vkpr" "loki.namespace" "NAMESPACE"
+  checkGlobalConfig "false" "false" "loki.metrics" "LOKI_METRICS"
+  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "loki.namespace" "LOKI_NAMESPACE"
 
   # External app values
-  checkGlobalConfig $VKPR_K8S_NAMESPACE "vkpr" "prometheus-stack.namespace" "GRAFANA_NAMESPACE"
+  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "prometheus-stack.namespace" "GRAFANA_NAMESPACE"
+
+  local VKPR_LOKI_VALUES; VKPR_LOKI_VALUES=$(dirname "$0")/utils/loki.yaml
 
   startInfos
   addRepLoki
@@ -26,22 +26,40 @@ addRepLoki(){
 }
 
 installLoki(){
-  local YQ_VALUES=".grafana.enabled = false"
   echoColor "bold" "$(echoColor "green" "Installing Loki...")"
+  local YQ_VALUES=".grafana.enabled = false"
   settingLoki
+
   $VKPR_YQ eval "$YQ_VALUES" "$VKPR_LOKI_VALUES" \
   | $VKPR_HELM upgrade -i --version "$VKPR_LOKI_VERSION" \
-    --create-namespace --namespace $VKPR_ENV_NAMESPACE\
+    --namespace "$VKPR_ENV_LOKI_NAMESPACE" --create-namespace \
     --wait -f - loki-stack grafana/loki-stack
 }
 
-existGrafana() {
-  if [[ $(checkPodName $VKPR_ENV_GRAFANA_NAMESPACE "prometheus-stack-grafana") = "true" ]]; then
-    local LOGINGRAFANA="$($VKPR_KUBECTL get secret --namespace $VKPR_ENV_GRAFANA_NAMESPACE prometheus-stack-grafana -o yaml \
-                          | $VKPR_YQ eval '.data.admin-user' - | base64 -d):$($VKPR_KUBECTL get secret --namespace $VKPR_ENV_GRAFANA_NAMESPACE prometheus-stack-grafana -o yaml | $VKPR_YQ eval '.data.admin-password' - | base64 -d)"
+settingLoki() {
+  if [[ "$VKPR_ENV_LOKI_METRICS" == true ]]; then
+    YQ_VALUES="$YQ_VALUES |
+      .loki.serviceMonitor.enabled = true |
+      .loki.serviceMonitor.interval = \"30s\" |
+      .loki.serviceMonitor.additionalLabels.release = \"prometheus-stack\" |
+      .loki.serviceMonitor.scrapeTimeout = \"30s\"
+    "
+  fi
 
-    local TOKEN_API_GRAFANA=$(curl -sk -X POST -H "Host: grafana.${VKPR_ENV_DOMAIN}" -H "Content-Type: application/json" \
-                              -d '{"name": "apikeycurl","role": "Admin"}' http://$LOGINGRAFANA@127.0.0.1:8000/api/auth/keys | $VKPR_JQ --raw-output '.key')
+  mergeVkprValuesHelmArgs "loki" "$VKPR_LOKI_VALUES"
+}
+
+existGrafana() {
+  if [[ $(checkPodName "$VKPR_ENV_GRAFANA_NAMESPACE" "prometheus-stack-grafana") == "true" ]]; then
+    local LOGINGRAFANA TOKEN_API_GRAFANA
+    LOGINGRAFANA="$($VKPR_KUBECTL get secret --namespace "$VKPR_ENV_GRAFANA_NAMESPACE" prometheus-stack-grafana -o yaml |\
+                    $VKPR_YQ eval '.data.admin-user' - | base64 -d):$($VKPR_KUBECTL get secret --namespace "$VKPR_ENV_GRAFANA_NAMESPACE" prometheus-stack-grafana -o yaml | $VKPR_YQ eval '.data.admin-password' - | base64 -d)"
+
+    TOKEN_API_GRAFANA=$(curl -skX POST \
+      -H "Host: grafana.${VKPR_ENV_DOMAIN}" -H "Content-Type: application/json" \
+      -d '{"name": "apikeycurl","role": "Admin"}'   
+      http://"$LOGINGRAFANA"@127.0.0.1:8000/api/auth/keys | $VKPR_JQ --raw-output '.key'
+    )
 
     if [[ $TOKEN_API_GRAFANA == "" ]]; then
       echoColor "red" "Api Token can only be request once or ingress is not installed."
@@ -54,23 +72,10 @@ existGrafana() {
     -d '{
           "name":"loki",
           "type":"loki",
-          "url":"loki-stack.'$VKPR_ENV_NAMESPACE'.svc.cluster.local:3100",
+          "url":"loki-stack.'"$VKPR_ENV_LOKI_NAMESPACE"'.svc.cluster.local:3100",
           "access":"proxy",
           "basicAuth":false,
           "editable": true
         }' http://127.0.0.1:8000/api/datasources
   fi
-}
-
-settingLoki() {
-  if [[ $VKPR_ENV_METRICS = "true" ]]; then
-    YQ_VALUES=''$YQ_VALUES' |
-      .loki.serviceMonitor.enabled = true |
-      .loki.serviceMonitor.interval = "30s" |
-      .loki.serviceMonitor.additionalLabels.release = "prometheus-stack" |
-      .loki.serviceMonitor.scrapeTimeout = "30s"
-    ' 
-  fi
-
-  mergeVkprValuesHelmArgs "loki" $VKPR_LOKI_VALUES
 }
--- vkpr/loki/remove/src/unix/formula/formula.sh
+++ vkpr/loki/remove/src/unix/formula/formula.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing Loki...")"
   
-  LOKI_NAMESPACE=$($VKPR_KUBECTL get po -A -l release=loki-stack,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall --namespace $LOKI_NAMESPACE loki-stack
+  LOKI_NAMESPACE=$($VKPR_KUBECTL get po -A -l release=loki-stack,vkpr=true -o=yaml |\
+                   $VKPR_YQ e ".items[].metadata.namespace" - |\
+                   head -n1)
+
+  $VKPR_HELM uninstall --namespace "$LOKI_NAMESPACE" loki-stack 2> /dev/null || echoColor "red" "VKPR Loki not found"
 }
