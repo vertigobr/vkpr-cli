276f34094d0db33b8ac86187d690767167ae96eb
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 15:50:34 -0300

    style(prometheus-stack): lint the code

--- vkpr/prometheus-stack/import/src/unix/formula/formula.sh
+++ vkpr/prometheus-stack/import/src/unix/formula/formula.sh
@@ -2,13 +2,18 @@
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Importing Dashboard...")"
-  local JSON_FILE=$(cat $DASHBOARD_PATH)
+  local JSON_FILE; JSON_FILE=$(cat "$DASHBOARD_PATH")
 
-  checkGlobalConfig $VKPR_K8S_NAMESPACE "vkpr" "prometheus-stack.namespace" "NAMESPACE"
+  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "prometheus-stack.namespace" "PROMETHEUS_STACK_NAMESPACE"
 
-  dashboard="$DASHBOARD_NAME-grafana" $VKPR_YQ eval '(.metadata.name, .metadata.labels.app) = strenv(dashboard)' $(dirname "$0")/utils/dashboard.yaml > $DASHBOARD_NAME.yaml
-  $VKPR_YQ eval -i '.metadata.namespace = "'$VKPR_ENV_NAMESPACE'"' $DASHBOARD_NAME.yaml
-  $VKPR_YQ eval '.data.["'$DASHBOARD_NAME.json'"] = ('"$JSON_FILE"' | to_json)' $DASHBOARD_NAME.yaml \
-  | $VKPR_KUBECTL apply -f -
-  rm $DASHBOARD_NAME.yaml
+  dashboard="$DASHBOARD_NAME-grafana" \
+    $VKPR_YQ eval '(.metadata.name, .metadata.labels.app) = strenv(dashboard)' \
+    "$(dirname "$0")"/utils/dashboard.yaml > "$DASHBOARD_NAME".yaml
+
+  $VKPR_YQ eval -i ".metadata.namespace = \"$VKPR_ENV_PROMETHEUS_STACK_NAMESPACE\"" "$DASHBOARD_NAME".yaml
+
+  $VKPR_YQ eval ".data.[\"$DASHBOARD_NAME.json\"] = (\"$JSON_FILE\" | to_json)" "$DASHBOARD_NAME".yaml |\
+   $VKPR_KUBECTL apply -f -
+
+  rm "$DASHBOARD_NAME".yaml
 }
--- vkpr/prometheus-stack/remove/src/unix/formula/formula.sh
+++ vkpr/prometheus-stack/remove/src/unix/formula/formula.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing Prometheus-stack...")"
 
-  PROMETHEUS_STACK_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=prometheus-stack,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall --namespace $PROMETHEUS_STACK_NAMESPACE prometheus-stack
-  $VKPR_KUBECTL delete cm -A -l grafana_dashboard=true,vkpr=true > /dev/null
-}
+  PROMETHEUS_STACK_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=prometheus-stack,vkpr=true -o=yaml |\
+                               $VKPR_YQ e ".items[].metadata.namespace" - |\
+                               head -n1)
+
+  $VKPR_KUBECTL delete cm -A --ignore-not-found=true -l grafana_dashboard=true,vkpr=true > /dev/null
+  $VKPR_HELM uninstall -n "$PROMETHEUS_STACK_NAMESPACE" prometheus-stack 2> /dev/null || echoColor "red" "VKPR Prometheus-stack not found"
+} 
