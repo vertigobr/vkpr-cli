c8df3e0ce98aa35c666ab4035d51787e2538c8f9
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 14:30:26 -0300

    style(gitlab): lint the code

--- vkpr/gitlab/parameter/add/src/unix/formula/formula.sh
+++ vkpr/gitlab/parameter/add/src/unix/formula/formula.sh
@@ -5,6 +5,6 @@ runFormula() {
   echoColor blue "Creating new parameter ${PARAMETER_NAME}"
   
   ## seting '*' as default value for PARAMETER_SCOPE
-  createOrUpdateVariable $PROJECT_ID $PARAMETER_NAME $PARAMETER_VALUE $PARAMETER_MASKED ${PARAMETER_SCOPE:-\*} $GITLAB_TOKEN
+  createOrUpdateVariable "$PROJECT_ID" "$PARAMETER_NAME" "$PARAMETER_VALUE" "$PARAMETER_MASKED" "${PARAMETER_SCOPE:-\*}" "$GITLAB_TOKEN"
 }
  
