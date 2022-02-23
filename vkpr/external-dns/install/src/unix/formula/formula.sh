832a1e9be0669d16c39beaa4778aa1735cfb0643
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 14:38:50 -0300

    style(external-dns): lint the code

--- vkpr/external-dns/install/src/unix/formula/formula.sh
+++ vkpr/external-dns/install/src/unix/formula/formula.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
-  local VKPR_EXTERNAL_DNS_VALUES=$(dirname "$0")/utils/external-dns.yaml
-  local RIT_CREDENTIALS_PATH=~/.rit/credentials/default
-  [[ $PDNS_APIURL == "" ]] && PDNS_APIURL="example.com"
+  [[ "$PDNS_APIURL" == "" ]] && PDNS_APIURL="example.com"
+  checkGlobalConfig "$PDNS_APIURL" "example.com" "external-dns.powerDNS.apiUrl" "EXTERNAL_DNS_PDNS_APIURL"
+  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "external-dns.namespace" "EXTERNAL_DNS_NAMESPACE"
+  checkGlobalConfig "false" "false" "external-dns.metrics" "EXTERNAL_DNS_METRICS"
 
-  checkGlobalConfig $PDNS_APIURL "example.com" "external-dns.powerDNS.apiUrl" "EXTERNAL_DNS_PDNS_APIURL"
-  checkGlobalConfig $VKPR_K8S_NAMESPACE "vkpr" "external-dns.namespace" "NAMESPACE"
-  checkGlobalConfig "false" "false" "external-dns.metrics" "METRICS"
+  local VKPR_EXTERNAL_DNS_VALUES; VKPR_EXTERNAL_DNS_VALUES="$(dirname "$0")"/utils/external-dns.yaml
+  local RIT_CREDENTIALS_PATH=~/.rit/credentials/default
 
   startInfos
   addRepoExternalDNS
@@ -26,15 +26,16 @@ addRepoExternalDNS() {
 }
 
 installExternalDNS() {
-  local YQ_VALUES='.rbac.create = true'
   if [[ ! -f $RIT_CREDENTIALS_PATH/$PROVIDER ]]; then
     echoColor "red" "Doesn't exists credential $PROVIDER to use in formula, create her or use the provider flag."
   else
     echoColor "bold" "$(echoColor "green" "Installing External-DNS...")"
+    local YQ_VALUES=".rbac.create = true"
     settingExternalDNS
+
     $VKPR_YQ eval "$YQ_VALUES" "$VKPR_EXTERNAL_DNS_VALUES" \
     | $VKPR_HELM upgrade -i --version "$VKPR_EXTERNAL_DNS_VERSION" \
-      --create-namespace --namespace "$VKPR_ENV_NAMESPACE" \
+      --namespace "$VKPR_ENV_EXTERNAL_DNS_NAMESPACE" --create-namespace \
       --wait -f - external-dns bitnami/external-dns
   fi
 }
@@ -43,44 +44,48 @@ installExternalDNS() {
 settingExternalDNS() {
   case $PROVIDER in
     aws)
-        AWS_REGION=$(cat ~/.rit/credentials/default/aws | $VKPR_JQ -r .credential.region)
-        AWS_ACCESS_KEY=$(cat ~/.rit/credentials/default/aws | $VKPR_JQ -r .credential.accesskeyid)
-        AWS_SECRET_KEY=$(cat ~/.rit/credentials/default/aws | $VKPR_JQ -r .credential.secretaccesskey)
-        validateAwsAccessKey $AWS_ACCESS_KEY
-        validateAwsSecretKey $AWS_SECRET_KEY
-        validateAwsRegion $AWS_REGION
-        YQ_VALUES=''$YQ_VALUES' |
-          .provider = "aws" |
-          .aws.credentials.accessKey = "'$AWS_ACCESS_KEY'" |
-          .aws.credentials.secretKey = "'$AWS_SECRET_KEY'" |
-          .aws.region = "'$AWS_REGION'"
-        '
+        AWS_REGION=$($VKPR_JQ -r .credential.region ~/.rit/credentials/default/aws)
+        AWS_ACCESS_KEY=$($VKPR_JQ -r .credential.accesskeyid ~/.rit/credentials/default/aws)
+        AWS_SECRET_KEY=$($VKPR_JQ -r .credential.secretaccesskey ~/.rit/credentials/default/aws)
+
+        validateAwsAccessKey "$AWS_ACCESS_KEY"
+        validateAwsSecretKey "$AWS_SECRET_KEY"
+        validateAwsRegion "$AWS_REGION"
+
+        YQ_VALUES="$YQ_VALUES |
+          .provider = \"aws\" |
+          .aws.credentials.accessKey = \"$AWS_ACCESS_KEY\" |
+          .aws.credentials.secretKey = \"$AWS_SECRET_KEY\" |
+          .aws.region = \"$AWS_REGION\"
+        "
       ;;
     digitalocean)
-        DO_TOKEN=$(cat ~/.rit/credentials/default/digitalocean | $VKPR_JQ -r .credential.token)
-        validateDigitalOceanApiToken $DO_TOKEN
-        YQ_VALUES=''$YQ_VALUES' |
-          .provider = "digitalocean" |
-          .digitalocean.apiToken = "'$DO_TOKEN'"
-        '
+        DO_TOKEN=$($VKPR_JQ -r .credential.token ~/.rit/credentials/default/digitalocean)
+        validateDigitalOceanApiToken "$DO_TOKEN"
+
+        YQ_VALUES="$YQ_VALUES |
+          .provider = \"digitalocean\" |
+          .digitalocean.apiToken = \"$DO_TOKEN\"
+        "
       ;;
     powerDNS)
-        YQ_VALUES=''$YQ_VALUES' |
-          .provider = "pdns" |
-          .pdns.apiUrl = "'$VKPR_ENV_EXTERNAL_DNS_PDNS_APIURL'" |
-          .pdns.apiKey = "'$($VKPR_JQ -r .credential.apikey $RIT_CREDENTIALS_PATH/powerDNS)'" |
-          .pdns.apiPort = "8081"
-        '
+        YQ_VALUES="$YQ_VALUES |
+          .provider = \"pdns\" |
+          .pdns.apiUrl = \"$VKPR_ENV_EXTERNAL_DNS_PDNS_APIURL\" |
+          .pdns.apiKey = \"$($VKPR_JQ -r .credential.apikey $RIT_CREDENTIALS_PATH/powerDNS)\" |
+          .pdns.apiPort = \"8081\"
+        "
       ;;
   esac
-  if [[ $VKPR_ENV_METRICS == "true" ]]; then
-    YQ_VALUES=''$YQ_VALUES' |
-      .metrics.enabled = true |
-      .metrics.serviceMonitor.enabled = true |
-      .metrics.serviceMonitor.namespace = "'$VKPR_ENV_NAMESPACE'" |
-      .metrics.serviceMonitor.interval = "1m"
-    '
+
+  if [[ $VKPR_ENV_EXTERNAL_DNS_METRICS == true ]]; then
+    YQ_VALUES="$YQ_VALUES |
+      .metrics.enabled = \"true\" |
+      .metrics.serviceMonitor.enabled = \"true\" |
+      .metrics.serviceMonitor.namespace = \"$VKPR_ENV_INGRESS_NAMESPACE\" |
+      .metrics.serviceMonitor.interval = \"1m\"
+    "
   fi
 
-  mergeVkprValuesHelmArgs "external-dns" $VKPR_EXTERNAL_DNS_VALUES
+  mergeVkprValuesHelmArgs "external-dns" "$VKPR_EXTERNAL_DNS_VALUES"
 }
--- vkpr/external-dns/remove/src/unix/formula/formula.sh
+++ vkpr/external-dns/remove/src/unix/formula/formula.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing External-DNS...")"
 
-  EXTERNAL_DNS_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=external-dns,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall external-dns -n $EXTERNAL_DNS_NAMESPACE
+  EXTERNAL_DNS_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=external-dns,vkpr=true -o=yaml |\
+                           $VKPR_YQ e ".items[].metadata.namespace" - |\
+                           head -n1)
+
+  $VKPR_HELM uninstall external-dns -n "$EXTERNAL_DNS_NAMESPACE" 2> /dev/null || echoColor "red" "VKPR external-dns not found"
 }
