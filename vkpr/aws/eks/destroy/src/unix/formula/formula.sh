e26db9f442c9631f3f4cc447ef30f82a96f733c1
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 15:51:38 -0300

    style(aws): lint the code

--- vkpr/aws/eks/destroy/src/unix/formula/formula.sh
+++ vkpr/aws/eks/destroy/src/unix/formula/formula.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
-  validateGitlabUsername $GITLAB_USERNAME
-  validateGitlabToken $GITLAB_TOKEN
+  validateGitlabUsername "$GITLAB_USERNAME"
+  validateGitlabToken "$GITLAB_TOKEN"
 
-  PROJECT_ID=$(curl https://gitlab.com/api/v4/users/$GITLAB_USERNAME/projects | $VKPR_JQ '.[0] | .id')
+  local PROJECT_ID; PROJECT_ID=$(curl https://gitlab.com/api/v4/users/"$GITLAB_USERNAME"/projects | $VKPR_JQ '.[0] | .id')
   destroyEKS
 }
 
 destroyEKS() {
-  DESTROY_ID=$(curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs | $VKPR_JQ '.[0] | .id')
-  curl -H "PRIVATE-TOKEN: $GITLAB_TOKEN" -X POST -s https://gitlab.com/api/v4/projects/$PROJECT_ID/jobs/$DESTROY_ID/play > /dev/null
+  DESTROY_ID=$(curl -H "PRIVATE-TOKEN: \"$GITLAB_TOKEN\"" https://gitlab.com/api/v4/projects/"$PROJECT_ID"/jobs | $VKPR_JQ '.[0] | .id')
+  curl -H "PRIVATE-TOKEN: \"$GITLAB_TOKEN\"" -X POST -s https://gitlab.com/api/v4/projects/"$PROJECT_ID"/jobs/"$DESTROY_ID"/play > /dev/null
 }
