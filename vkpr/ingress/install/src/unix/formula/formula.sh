31ce529825f5c879870e264470e35199b4413498
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 14:41:21 -0300

    style(ingress): lint the code

--- vkpr/ingress/install/src/unix/formula/formula.sh
+++ vkpr/ingress/install/src/unix/formula/formula.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 
 runFormula() {
-  local VKPR_INGRESS_VALUES=$(dirname "$0")/utils/ingress.yaml
-  
-  checkGlobalConfig $LB_TYPE "Classic" "ingress.loadBalancerType" "LB_TYPE"
-  checkGlobalConfig "false" "false" "ingress.metrics" "METRICS"
-  checkGlobalConfig $VKPR_K8S_NAMESPACE "vkpr" "ingress.namespace" "NAMESPACE"
+  checkGlobalConfig "$LB_TYPE" "Classic" "ingress.loadBalancerType" "INGRESS_LB_TYPE"
+  checkGlobalConfig "false" "false" "ingress.metrics" "INGRESS_METRICS"
+  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "ingress.namespace" "INGRESS_NAMESPACE"
 
+  local VKPR_INGRESS_VALUES; VKPR_INGRESS_VALUES="$(dirname "$0")"/utils/ingress.yaml
+  
   startInfos
   configureRepository
   installIngress
@@ -27,31 +27,32 @@ installIngress() {
   echoColor "bold" "$(echoColor "green" "Installing ngnix ingress...")"
   local YQ_VALUES=".rbac.create = true"
   settingIngress
+
   $VKPR_YQ eval "$YQ_VALUES" "$VKPR_INGRESS_VALUES" \
   | $VKPR_HELM upgrade -i --version "$VKPR_INGRESS_NGINX_VERSION" \
-      --namespace $VKPR_ENV_NAMESPACE --create-namespace \
-      --wait --timeout 5m0s -f - ingress-nginx ingress-nginx/ingress-nginx
+      --namespace "$VKPR_ENV_INGRESS_NAMESPACE" --create-namespace \
+      --wait -f - ingress-nginx ingress-nginx/ingress-nginx
 }
 
 settingIngress() {
-  if [[ $VKPR_ENV_LB_TYPE == "NLB" ]]; then
-    YQ_VALUES='
-      .controller.service.annotations.["'service.beta.kubernetes.io/aws-load-balancer-backend-protocol'"] = "tcp" |
-      .controller.service.annotations.["'service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled'"] = "'true'" |
-      .controller.service.annotations.["'service.beta.kubernetes.io/aws-load-balancer-type'"] = "nlb"
-    '
+  if [[ "$VKPR_ENV_INGRESS_LB_TYPE" == "NLB" ]]; then
+    YQ_VALUES="
+      .controller.service.annotations.[\"service.beta.kubernetes.io/aws-load-balancer-backend-protocol\"] = \"tcp\" |
+      .controller.service.annotations.[\"service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled\"] = \"true\" |
+      .controller.service.annotations.[\"service.beta.kubernetes.io/aws-load-balancer-type\"] = \"nlb\"
+    "
   fi
 
-  if [[ $VKPR_ENV_METRICS = "true" ]]; then
-    YQ_VALUES=''$YQ_VALUES' |
-      .controller.metrics.enabled = "true" |
-      .controller.metrics.service.annotations.["'prometheus.io/scrape'"] = "true" |
-      .controller.metrics.service.annotations.["'prometheus.io/port'"] = "10254" |
-      .controller.metrics.serviceMonitor.enabled = "true" |
-      .controller.metrics.serviceMonitor.namespace = "'$VKPR_ENV_NAMESPACE'" |
-      .controller.metrics.serviceMonitor.additionalLabels.release = "prometheus-stack"
-    ' 
+  if [[ "$VKPR_ENV_INGRESS_METRICS" == "true" ]]; then
+    YQ_VALUES="$YQ_VALUES |
+      .controller.metrics.enabled = true |
+      .controller.metrics.service.annotations.[\"prometheus.io/scrape\"] = \"true\" |
+      .controller.metrics.service.annotations.[\"prometheus.io/port\"] = \"10254\" |
+      .controller.metrics.serviceMonitor.enabled = true |
+      .controller.metrics.serviceMonitor.namespace = \"$VKPR_ENV_INGRESS_NAMESPACE\" |
+      .controller.metrics.serviceMonitor.additionalLabels.release = \"prometheus-stack\"
+     "
   fi
 
-  mergeVkprValuesHelmArgs "ingress" $VKPR_INGRESS_VALUES
+  mergeVkprValuesHelmArgs "ingress" "$VKPR_INGRESS_VALUES"
 }
--- vkpr/ingress/remove/src/unix/formula/formula.sh
+++ vkpr/ingress/remove/src/unix/formula/formula.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 runFormula() {
   echoColor "bold" "$(echoColor "green" "Removing nginx ingress...")"
 
-  INGRESS_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=ingress-nginx,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall --namespace $INGRESS_NAMESPACE ingress-nginx || echoColor "red" "VKPR Ingress not found"
+  INGRESS_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=ingress-nginx,vkpr=true -o=yaml |\
+                      $VKPR_YQ e ".items[].metadata.namespace" - |\
+                      head -n1)
+
+  $VKPR_HELM uninstall --namespace "$INGRESS_NAMESPACE" ingress-nginx 2> /dev/null | echoColor "red" "VKPR ingress-nginx not found"
 }
