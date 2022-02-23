ce310caeddd7d70d2463731cf45d411b156184bf
Jpeedroza <pedrozajoaopedro@gmail.com>
2022-02-23 15:57:53 -0300

    feat(infra): create new formula infra stop and change the infra down

---
+++ vkpr/infra/stop/src/unix/formula/formula.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+runFormula() {
+  echo "=============================="
+  echoColor "bold" "$(echoColor "green" "VKPR Local Infra Stop Routine")"
+  echo "=============================="
+  stopCluster
+}
+
+stopCluster() {
+  if $(${VKPR_K3D} cluster list | grep -q "vkpr-local"); then
+    ${VKPR_K3D} cluster delete vkpr-local
+  else
+    echoColor "bold" "$(echoColor "red" "Cluster vkpr-local not running...")"
+  fi
+  if [[ $DELETE_REGISTRY == "true" ]]; then
+    docker rm -f k3d-mirror.localhost k3d-registry.localhost > /dev/null
+  fi
+}
