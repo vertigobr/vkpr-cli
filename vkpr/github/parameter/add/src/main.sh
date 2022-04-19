1af64cd08ffbdd5a12e7f616eae2e0078ec92116
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2022-04-19 15:24:23 -0300

    new command to add github secret

---
+++ vkpr/github/parameter/add/src/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source src/log.sh
+source src/var.sh
+source src/helper.sh
+source src/validate.sh
+source src/versions.sh
+source src/github-operations.sh
+
+. "$(dirname "$0")"/unix/formula/formula.sh --source-only
+
+runFormula
---
+++ vkpr/github/parameter/add/src/unix/formula/formula.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+runFormula() {
+
+  echoColor blue "Creating new secret ${PARAMETER_NAME}"
+  
+  #getting public key to encrypt secret value
+  PUBLIC_KEY=$(githubActionsGetPublicKey $PROJECT_NAME $GITHUB_USERNAME $GITHUB_TOKEN)
+  
+  #calling function to create or update secret
+  githubActionsCreateUpdateSecret $PROJECT_NAME $SECRET_NAME $SECRET_VALUE $PUBLIC_KEY $GITHUB_USERNAME $GITHUB_TOKEN
+  
+}
