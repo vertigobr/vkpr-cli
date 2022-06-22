dd73e481fd92b0c8543d12741192bba776dfe567
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-06-22 11:52:57 -0300

    fix(mockserver): mockserver label doesnt exists

--- vkpr/mockserver/remove/src/unix/formula/formula.sh
+++ vkpr/mockserver/remove/src/unix/formula/formula.sh
@@ -3,7 +3,7 @@
 runFormula() {
   info "Removing mockserver..."
 
-  MOCKSERVER_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=mockserver,vkpr=true -o=yaml |\
+  MOCKSERVER_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=mockserver -o=yaml |\
                      $VKPR_YQ e ".items[].metadata.namespace" - |\
                      head -n1)
 
