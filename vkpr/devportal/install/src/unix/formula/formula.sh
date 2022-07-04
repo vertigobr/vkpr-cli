221376cccd0965688a8ba3bc6a68352cd84b6993
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-04 17:59:10 -0300

    fix(devportal): update formula structure

--- vkpr/devportal/install/src/unix/formula/formula.sh
+++ vkpr/devportal/install/src/unix/formula/formula.sh
@@ -1,63 +1,44 @@
 #!/bin/bash
 
 runFormula() {
-  # Global values
-  checkGlobalConfig "" "" "global.provider" "GLOBAL_PROVIDER"
-  checkGlobalConfig "$DOMAIN" "localhost" "global.domain" "GLOBAL_DOMAIN"
-  checkGlobalConfig "$SECURE" "false" "global.secure" "GLOBAL_SECURE"
-  checkGlobalConfig "nginx" "nginx" "global.ingressClassName" "GLOBAL_INGRESS_CLASS_NAME"
-  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "global.namespace" "GLOBAL_NAMESPACE"
+  local VKPR_ENV_DEVPORTAL_DOMAIN VKPR_DEVPORTAL_VALUES RIT_CREDENTIALS_PATH HELM_ARGS;
+  formulaInputs
+  validateInputs
 
-  # App values
-  checkGlobalConfig "$VKPR_ENV_GLOBAL_INGRESS_CLASS_NAME" "$VKPR_ENV_GLOBAL_INGRESS_CLASS_NAME" "devportal.ingressClassName" "DEVPORTAL_INGRESS_CLASS_NAME"
-  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "devportal.namespace" "DEVPORTAL_NAMESPACE"
-
-  local VKPR_ENV_DEVPORTAL_DOMAIN="devportal.${VKPR_ENV_GLOBAL_DOMAIN}" \
-        RIT_CREDENTIALS_PATH=~/.rit/credentials/default
-  local VKPR_DEVPORTAL_VALUES; VKPR_DEVPORTAL_VALUES=$(dirname "$0")/utils/devportal.yaml
-  local HELM_NAMESPACE="--namespace=$VKPR_ENV_DEVPORTAL_NAMESPACE --create-namespace"
+  VKPR_ENV_DEVPORTAL_DOMAIN="devportal.${VKPR_ENV_GLOBAL_DOMAIN}"
+  VKPR_DEVPORTAL_VALUES=$(dirname "$0")/utils/devportal.yaml
+  RIT_CREDENTIALS_PATH=~/.rit/credentials/default
 
   startInfos
-  addRepoDevportal
-  installDevportal
+  settingDevportal
+  [ $DRY_RUN = false ] && registerHelmRepository veecode-platform https://vfipaas.github.io/public-charts/
+  installApplication "devportal" "veecode-platform/devportal" "$VKPR_ENV_DEVPORTAL_NAMESPACE" "$VKPR_DEVPORTAL_VERSION" "$VKPR_DEVPORTAL_VALUES" "$HELM_ARGS"
 }
 
 startInfos() {
-  echo "=============================="
-  info "VKPR Devportal Install Routine"
-  notice "Devportal Domain: $VKPR_ENV_DEVPORTAL_DOMAIN"
-  notice "Ingress Controller: $VKPR_ENV_DEVPORTAL_INGRESS_CLASS_NAME"
-  echo "=============================="
+  bold "=============================="
+  boldInfo "VKPR Devportal Install Routine"
+  boldNotice "Domain: $VKPR_ENV_DEVPORTAL_DOMAIN"
+  boldNotice "Secure: $VKPR_ENV_GLOBAL_SECURE"
+  boldNotice "Namespace: $VKPR_ENV_DEVPORTAL_NAMESPACE"
+  boldNotice "Ingress Controller: $VKPR_ENV_DEVPORTAL_INGRESS_CLASS_NAME"
+  bold "=============================="
 }
 
-addRepoDevportal() {
-  registerHelmRepository veecode-platform https://vfipaas.github.io/public-charts/
+formulaInputs() {
+  # App values
+  checkGlobalConfig "$VKPR_ENV_GLOBAL_INGRESS_CLASS_NAME" "$VKPR_ENV_GLOBAL_INGRESS_CLASS_NAME" "devportal.ingressClassName" "DEVPORTAL_INGRESS_CLASS_NAME"
+  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "devportal.namespace" "DEVPORTAL_NAMESPACE"
 }
 
-installDevportal() {
-  local YQ_VALUES=".ingress.hosts[0].host = \"$VKPR_ENV_DEVPORTAL_DOMAIN\" |
-   .ingress.hosts[0].paths[0].path = \"/\"|
-    .ingress.hosts[0].paths[0].pathType = \"ImplementationSpecific\"
-  "
-  settingDevportal
-
-  if [[ $DRY_RUN == true ]]; then
-    bold "---"
-    mergeVkprValuesHelmArgs "devportal" "$VKPR_DEVPORTAL_VALUES"  
-    $VKPR_YQ eval "$YQ_VALUES" "$VKPR_DEVPORTAL_VALUES"  
-  else
-    info "Installing devportal..."
-    $VKPR_YQ eval -i "$YQ_VALUES" "$VKPR_DEVPORTAL_VALUES"
-    mergeVkprValuesHelmArgs "devportal" "$VKPR_DEVPORTAL_VALUES"
-    # shellcheck disable=SC2086
-    $VKPR_HELM upgrade -i --version "$VKPR_DEVPORTAL_VERSION" $HELM_NAMESPACE \
-      --wait -f "$VKPR_DEVPORTAL_VALUES" devportal veecode-platform/devportal
-  fi
+validateInputs() {
 }
 
 settingDevportal() {
-  YQ_VALUES="$YQ_VALUES |
-    .ingress.enabled = true |
+  local YQ_VALUES=".ingress.enabled = true |
+    .ingress.hosts[0].host = \"$VKPR_ENV_DEVPORTAL_DOMAIN\" |
+    .ingress.hosts[0].paths[0].path = \"/\"|
+    .ingress.hosts[0].paths[0].pathType = \"ImplementationSpecific\" |
     .ingress.ingressClassName = \"$VKPR_ENV_DEVPORTAL_INGRESS_CLASS_NAME\" |
     .appConfig.app.baseUrl = \"http://$VKPR_ENV_DEVPORTAL_DOMAIN/\" |
     .appConfig.backend.baseUrl = \"http://$VKPR_ENV_DEVPORTAL_DOMAIN/\" |
@@ -77,16 +58,16 @@ settingDevportal() {
   fi
 
   settingDevportalProvider
+
+  debug "YQ_CONTENT = $YQ_VALUES"
 }
 
 settingDevportalProvider() {
-  ACTUAL_CONTEXT=$($VKPR_KUBECTL config get-contexts --no-headers | grep "\*" | xargs | awk -F " " '{print $2}')
-  if [[ "$VKPR_ENV_GLOBAL_PROVIDER" == "okteto" ]] || [[ $ACTUAL_CONTEXT == "cloud_okteto_com" ]]; then
-    OKTETO_NAMESPACE=$($VKPR_KUBECTL config get-contexts --no-headers | grep "\*" | xargs | awk -F " " '{print $NF}')
-    HELM_NAMESPACE=""
+  if [[ "$VKPR_ENVIRONMENT" == "okteto" ]]; then
+    HELM_ARGS="--cleanup-on-fail"
     YQ_VALUES="$YQ_VALUES |
-      .ingress.enabled = \"false\" |
+      .ingress.enabled = false |
       .service.annotations.[\"dev.okteto.com/auto-ingress\"] = \"true\"
     "
   fi
-} 
+}
