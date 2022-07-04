c4155c9ee6c0f5c3d307c062f0a94f8142a5b820
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-04 15:18:12 -0300

    fix(consul): update formula structure

--- vkpr/consul/install/src/unix/formula/formula.sh
+++ vkpr/consul/install/src/unix/formula/formula.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
 runFormula() {
-  # Global values
-  checkGlobalConfig "$DOMAIN" "localhost" "global.domain" "GLOBAL_DOMAIN"
-  checkGlobalConfig "$SECURE" "false" "global.secure" "GLOBAL_SECURE"
-  checkGlobalConfig "nginx" "nginx" "global.ingressClassName" "GLOBAL_INGRESS"
-  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "global.namespace" "GLOBAL_NAMESPACE"
-  
+  local VKPR_ENV_CONSUL_DOMAIN VKPR_CONSUL_VALUES HELM_ARGS;
+  formulaInputs
+  validateInputs
+
+  VKPR_ENV_CONSUL_DOMAIN="consul.${VKPR_ENV_GLOBAL_DOMAIN}"
+  VKPR_CONSUL_VALUES=$(dirname "$0")/utils/consul.yaml
+
+  startInfos
+  settingConsul
+  [ $DRY_RUN = false ] && registerHelmRepository hashicorp https://helm.releases.hashicorp.com
+  installApplication "consul" "hashicorp/consul" "$VKPR_ENV_CONSUL_NAMESPACE" "$VKPR_CONSUL_VERSION" "$VKPR_CONSUL_VALUES" "$HELM_ARGS"
+}
+
+startInfos() {
+  bold "=============================="
+  boldInfo "VKPR Consul Install Routine"
+  boldNotice "Domain: $VKPR_ENV_CONSUL_DOMAIN"
+  boldNotice "Secure: $VKPR_ENV_GLOBAL_SECURE"
+  boldNotice "Namespace: $VKPR_ENV_CONSUL_NAMESPACE"
+  boldNotice "Ingress Controller: $VKPR_ENV_CONSUL_INGRESS_CLASS_NAME"
+  bold "=============================="
+}
+
+formulaInputs() {
   # App values
   checkGlobalConfig "$VKPR_ENV_GLOBAL_INGRESS" "$VKPR_ENV_GLOBAL_INGRESS" "consul.ingressClassName" "CONSUL_INGRESS_CLASS_NAME"
   checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "consul.namespace" "CONSUL_NAMESPACE"
@@ -14,47 +32,17 @@ runFormula() {
   checkGlobalConfig "$CRT_FILE" "" "consul.ssl.crt" "CONSUL_CERTIFICATE"
   checkGlobalConfig "$KEY_FILE" "" "consul.ssl.key" "CONSUL_KEY"
   checkGlobalConfig "" "" "consul.ssl.secretName" "CONSUL_SSL_SECRET"
-
-  local VKPR_ENV_CONSUL_DOMAIN="consul.${VKPR_ENV_GLOBAL_DOMAIN}"
-  local VKPR_CONSUL_VALUES; VKPR_CONSUL_VALUES="$(dirname "$0")"/utils/consul.yaml
-  
-  startInfos
-  configureRepository
-  installConsul
 }
 
-startInfos() {
-  echo "=============================="
-  bold "$(info "VKPR Consul Install Routine")"
-  bold "$(notice "Consul UI Domain:") ${VKPR_ENV_CONSUL_DOMAIN}"
-  bold "$(notice "Consul UI HTTPS:") ${VKPR_ENV_GLOBAL_SECURE}"
-  bold "$(notice "Ingress Controller:") ${VKPR_ENV_CONSUL_INGRESS_CLASS_NAME}"
-  echo "=============================="
-}
+validateInputs() {
 
-configureRepository() {
-  registerHelmRepository hashicorp https://helm.releases.hashicorp.com
-}
-
-installConsul() {
-  local YQ_VALUES=".ui.ingress.hosts[0].host = \"$VKPR_ENV_CONSUL_DOMAIN\" | .ui.ingress.ingressClassName = \"$VKPR_ENV_CONSUL_INGRESS_CLASS_NAME\""
-  settingConsul
-
-  if [[ $DRY_RUN == true ]]; then
-    echoColor "bold" "---"
-    mergeVkprValuesHelmArgs "consul" "$VKPR_CONSUL_VALUES"
-    $VKPR_YQ eval "$YQ_VALUES" "$VKPR_CONSUL_VALUES"    
-  else
-    bold "$(info "Installing Consul...")" 
-    $VKPR_YQ eval -i "$YQ_VALUES" "$VKPR_CONSUL_VALUES"
-    mergeVkprValuesHelmArgs "consul" "$VKPR_CONSUL_VALUES"
-    $VKPR_HELM upgrade -i --version "$VKPR_CONSUL_VERSION" \
-      --namespace "$VKPR_ENV_CONSUL_NAMESPACE" --create-namespace \
-      --wait -f "$VKPR_CONSUL_VALUES" consul hashicorp/consul
-  fi
 }
 
 settingConsul() {
+  YQ_VALUES=".ui.ingress.hosts[0].host = \"$VKPR_ENV_CONSUL_DOMAIN\" |
+   .ui.ingress.ingressClassName = \"$VKPR_ENV_CONSUL_INGRESS_CLASS_NAME\"
+  "
+
   if [[ "$VKPR_ENV_GLOBAL_SECURE" == true ]]; then
     YQ_VALUES="$YQ_VALUES |
       .ui.ingress.tls[0].hosts[0] = \"$VKPR_ENV_CONSUL_DOMAIN\" |
@@ -77,5 +65,16 @@ settingConsul() {
       .ui.ingress.tls[0].hosts[0] = \"$VKPR_ENV_CONSUL_DOMAIN\" |
       .ui.ingress.tls[0].secretName = \"$VKPR_ENV_CONSUL_SSL_SECRET\"
      "
+  fi
+  
+  settingConsulEnvironment
+
+  debug "YQ_CONTENT = $YQ_VALUES"
+}
+
+settingConsulEnvironment() {
+  if [[ "$VKPR_ENVIRONMENT" == "okteto" ]]; then
+    HELM_ARGS="--cleanup-on-fail"
+    #YQ_VALUES="$YQ_VALUES"
   fi
 }
