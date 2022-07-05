80275aebbb7fb186273e2a5e7369305784e5d9f3
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-05 19:06:47 -0300

    fix(cert-manager): update formula structure

--- lib/var.sh
+++ lib/var.sh
@@ -2,7 +2,8 @@
 
 export VKPR_HOME=~/.vkpr \
   VKPR_CONFIG=~/.vkpr/config \
-  VKPR_FILE=$CURRENT_PWD/vkpr.yaml
+  VKPR_FILE=$CURRENT_PWD/vkpr.yaml \
+  VKPR_CREDENTIAL=~/.rit/credentials/default
 
 export VKPR_GLAB=$VKPR_HOME/bin/glab \
   VKPR_K3D=$VKPR_HOME/bin/k3d \
