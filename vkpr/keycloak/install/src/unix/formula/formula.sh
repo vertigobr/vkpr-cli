9fefdd19bc827ffa3b18b0711598fcfba52944cc
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-06 17:03:39 -0300

    fix(keycloak): update formula structure

--- vkpr/keycloak/install/src/unix/formula/formula.sh
+++ vkpr/keycloak/install/src/unix/formula/formula.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
 runFormula() {
-  local PG_USER="postgres"
-  local PG_PASSWORD; PG_PASSWORD=$($VKPR_JQ -r '.credential.password' "$VKPR_CREDENTIAL"/postgres)
-  local PG_DATABASE_NAME="keycloak"
-  local VKPR_KEYCLOAK_VALUES; VKPR_KEYCLOAK_VALUES=$(dirname "$0")/utils/keycloak.yaml
+  local VKPR_ENV_KEYCLOAK_DOMAIN VKPR_KEYCLOAK_VALUES PG_USER PG_DATABASE_NAME PG_HA PG_HOST HELM_ARGS;
+  formulaInputs
+  validateInputs
 
-  # Global values
-  checkGlobalConfig "$DOMAIN" "localhost" "global.domain" "GLOBAL_DOMAIN"
-  checkGlobalConfig "$SECURE" "false" "global.secure" "GLOBAL_SECURE"
-  checkGlobalConfig "nginx" "nginx" "global.ingressClassName" "GLOBAL_INGRESS"
-  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "global.namespace" "GLOBAL_NAMESPACE"
-  
+  VKPR_ENV_KEYCLOAK_DOMAIN="keycloak.${VKPR_ENV_GLOBAL_DOMAIN}"
+  VKPR_KEYCLOAK_VALUES=$(dirname "$0")/utils/keycloak.yaml
+
+  startInfos
+  if [ $DRY_RUN = false ]; then
+    configureKeycloakDB
+    registerHelmRepository bitnami https://charts.bitnami.com/bitnami
+  fi
+  settingKeycloak
+  installApplication "keycloak" "bitnami/keycloak" "$VKPR_ENV_KEYCLOAK_INGRESS_CLASS_NAME" "$VKPR_KEYCLOAK_VERSION" "$VKPR_KEYCLOAK_VALUES" "$HELM_ARGS"
+}
+
+startInfos() {
+  bold "=============================="
+  boldInfo "VKPR Keycloak Install Routine"
+  boldNotice "Domain: $VKPR_ENV_KEYCLOAK_DOMAIN"
+  boldNotice "Secure: $VKPR_ENV_GLOBAL_SECURE"
+  boldNotice "Namespace: $VKPR_ENV_WHOAMI_NAMESPACE"
+  boldNotice "HA: $VKPR_ENV_HA"
+  boldNotice "Ingress Controller: $VKPR_ENV_WHOAMI_INGRESS_CLASS_NAME"
+  boldNotice "Keycloak Admin Username: $VKPR_ENV_KEYCLOAK_ADMIN_USER"
+  boldNotice "Keycloak Admin Password: $VKPR_ENV_KEYCLOAK_ADMIN_PASSWORD"
+  bold "=============================="
+}
+
+formulaInputs() {
   # App values
-  checkGlobalConfig "$HA" "false" "keycloak.HA" "HA"
+  checkGlobalConfig "$HA" "false" "keycloak.HA" "KEYCLOAK_HA"
   checkGlobalConfig "$ADMIN_USER" "admin" "keycloak.adminUser" "KEYCLOAK_ADMIN_USER"
   checkGlobalConfig "$ADMIN_PASSWORD" "vkpr123" "keycloak.adminPassword" "KEYCLOAK_ADMIN_PASSWORD"
   checkGlobalConfig "$VKPR_ENV_GLOBAL_INGRESS" "$VKPR_ENV_GLOBAL_INGRESS" "keycloak.ingressClassName" "KEYCLOAK_INGRESS_CLASS_NAME"
-  checkGlobalConfig "false" "false" "keycloak.metrics" "METRICS"
-  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "keycloak.namespace" "NAMESPACE"
+  checkGlobalConfig "false" "false" "keycloak.metrics" "KEYCLOAK_METRICS"
+  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "keycloak.namespace" "KEYCLOAK_NAMESPACE"
   checkGlobalConfig "$SSL" "false" "keycloak.ssl.enabled" "KEYCLOAK_SSL"
   checkGlobalConfig "$CRT_FILE" "" "keycloak.ssl.crt" "KEYCLOAK_CERTIFICATE"
   checkGlobalConfig "$KEY_FILE" "" "keycloak.ssl.key" "KEYCLOAK_KEY"
@@ -26,75 +45,65 @@ runFormula() {
 
   # External apps values
   checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "postgresql.namespace" "POSTGRESQL_NAMESPACE"
-
-  local VKPR_ENV_KEYCLOAK_DOMAIN="keycloak.${VKPR_ENV_GLOBAL_DOMAIN}"
-
-  startInfos
-  addRepoKeycloak
-  installKeycloak
 }
 
-startInfos() {
-  echo "=============================="
-  bold "$(info "VKPR Keycloak Install Routine")"
-  bold "$(notice "Keycloak Domain:") ${VKPR_ENV_KEYCLOAK_DOMAIN}"
-  bold "$(notice "Keycloak HTTPS:") ${VKPR_ENV_GLOBAL_SECURE}"
-  bold "$(notice "HA:") ${VKPR_ENV_HA}"
-  bold "$(notice "Keycloak Admin Username:") ${VKPR_ENV_KEYCLOAK_ADMIN_USER}"
-  bold "$(notice "Keycloak Admin Password:") ${VKPR_ENV_KEYCLOAK_ADMIN_PASSWORD}"
-  bold "$(notice "Ingress Controller:") ${VKPR_ENV_KEYCLOAK_INGRESS_CLASS_NAME}"
-  echo "=============================="
-}
+#validateInputs() {}
 
-addRepoKeycloak(){
-  registerHelmRepository bitnami https://charts.bitnami.com/bitnami
-}
+configureKeycloakDB(){
+  PG_USER="postgres"
+  PG_DATABASE_NAME="keycloak"
 
-installKeycloak(){
-  local YQ_VALUES=".postgresql.enabled = false"
-  [[ $DRY_RUN == false ]] && configureKeycloakDB
-  settingKeycloak
-  bold "$(info "Installing Keycloak...")"
+  PG_HOST="postgres-postgresql.$VKPR_ENV_POSTGRESQL_NAMESPACE"
+  $VKPR_KUBECTL get pod -n "$VKPR_ENV_POSTGRESQL_NAMESPACE" | grep -q pgpool && PG_HOST="postgres-postgresql-pgpool.$VKPR_ENV_POSTGRESQL_NAMESPACE"
 
-  if [[ $DRY_RUN == true ]]; then
-    bold "---"
-    mergeVkprValuesHelmArgs "keycloak" "$VKPR_KEYCLOAK_VALUES"
-    $VKPR_YQ eval "$YQ_VALUES" "$VKPR_KEYCLOAK_VALUES"
-  else
-    bold "$(info "Installing Keycloak...")"
-    $VKPR_YQ eval -i "$YQ_VALUES" "$VKPR_KEYCLOAK_VALUES"
-    mergeVkprValuesHelmArgs "keycloak" "$VKPR_KEYCLOAK_VALUES"
-    $VKPR_HELM upgrade -i --version "$VKPR_KEYCLOAK_VERSION" \
-      --create-namespace -n "$VKPR_ENV_NAMESPACE" \
-      --wait --timeout 10m -f "$VKPR_KEYCLOAK_VALUES" keycloak bitnami/keycloak
+  PG_HA="false"
+  [[ $VKPR_ENV_KEYCLOAK_HA == true ]] && PG_HA="true"
+
+  if [[ $(checkPodName "$VKPR_ENV_POSTGRESQL_NAMESPACE" "postgres-postgresql") != "true" ]]; then
+    info "Initializing postgresql to Keycloak"
+    [[ -f $CURRENT_PWD/vkpr.yaml ]] && cp "$CURRENT_PWD"/vkpr.yaml "$(dirname "$0")"
+    rit vkpr postgresql install --HA="$PG_HA" --default
+  fi
+
+  if [[ $(checkExistingDatabase "$PG_USER" "$PG_PASSWORD" "$PG_DATABASE_NAME" "$VKPR_ENV_POSTGRESQL_NAMESPACE") != "keycloak" ]]; then
+    info "Creating Database Instance in postgresql..."
+    createDatabase "$PG_USER" "$PG_PASSWORD" "$PG_DATABASE_NAME" "$VKPR_ENV_POSTGRESQL_NAMESPACE"
   fi
 }
 
 settingKeycloak(){
-  YQ_VALUES="$YQ_VALUES |
+  YQ_VALUES=".postgresql.enabled = false |
     .ingress.hostname = \"$VKPR_ENV_KEYCLOAK_DOMAIN\" |
     .ingress.ingressClassName = \"$VKPR_ENV_KEYCLOAK_INGRESS_CLASS_NAME\" |
     .auth.adminUser = \"$VKPR_ENV_KEYCLOAK_ADMIN_USER\" |
     .auth.adminPassword = \"$VKPR_ENV_KEYCLOAK_ADMIN_PASSWORD\" |
-    .proxy = \"none\"
+    .proxy = \"none\" |
+    .externalDatabase.host = \"$PG_HOST\" |
+    .externalDatabase.port = \"5432\" |
+    .externalDatabase.user = \"$PG_USER\" |
+    .externalDatabase.password = \"$PG_PASSWORD\" |
+    .externalDatabase.database = \"$PG_DATABASE_NAME\"
   "
+
   if [[ $VKPR_ENV_GLOBAL_SECURE = true ]]; then
     YQ_VALUES="$YQ_VALUES |
       .ingress.annotations.[\"kubernetes.io/tls-acme\"] = \"true\" |
       .ingress.tls = true
     "
   fi
-  if [[ $VKPR_ENV_HA = true ]]; then
+
+  if [[ $VKPR_ENV_KEYCLOAK_HA = true ]]; then
     YQ_VALUES="$YQ_VALUES |
       .replicaCount = 3 |
       .cache.enabled = true
     "
   fi
-  if [[ $VKPR_ENV_METRICS == "true" ]]; then
+
+  if [[ $VKPR_ENV_KEYCLOAK_METRICS == "true" ]]; then
     YQ_VALUES="$YQ_VALUES |
       .metrics.enabled = true |
       .metrics.serviceMonitor.enabled = true |
-      .metrics.serviceMonitor.namespace = \"$VKPR_ENV_GLOBAL_NAMESPACE\" |
+      .metrics.serviceMonitor.namespace = \"$VKPR_ENV_KEYCLOAK_NAMESPACE\" |
       .metrics.serviceMonitor.interval = \"30s\" |
       .metrics.serviceMonitor.scrapeTimeout = \"30s\" |
       .metrics.serviceMonitor.labels.release = \"prometheus-stack\" 
@@ -114,29 +123,4 @@ settingKeycloak(){
       .ingress.secrets[0].certificate = \"$KEYCLOAK_TLS_CERT\"
      "
   fi
-}
-
-configureKeycloakDB(){
-  local PG_HA="false"
-  validatePostgresqlPassword "$PASSWORD"
-  [[ $VKPR_ENV_HA == true ]] && PG_HA="true"
-  if [[ $(checkPodName "$VKPR_ENV_POSTGRESQL_NAMESPACE" "postgres-postgresql") != "true" ]]; then
-    info "Initializing postgresql to Keycloak"
-    [[ -f $CURRENT_PWD/vkpr.yaml ]] && cp "$CURRENT_PWD"/vkpr.yaml "$(dirname "$0")"
-    rit vkpr postgresql install --HA="$PG_HA" --default
-  fi
-  if [[ $(checkExistingDatabase "$PG_USER" "$PG_PASSWORD" "$PG_DATABASE_NAME" "$VKPR_ENV_POSTGRESQL_NAMESPACE") != "keycloak" ]]; then
-    info "Creating Database Instance in postgresql..."
-    createDatabase "$PG_USER" "$PG_PASSWORD" "$PG_DATABASE_NAME" "$VKPR_ENV_POSTGRESQL_NAMESPACE"
-  fi
-
-  local PG_HOST="postgres-postgresql.$VKPR_ENV_POSTGRESQL_NAMESPACE"
-  $VKPR_KUBECTL get pod -n "$VKPR_ENV_POSTGRESQL_NAMESPACE" | grep -q pgpool && PG_HOST="postgres-postgresql-pgpool.$VKPR_ENV_POSTGRESQL_NAMESPACE"
-  YQ_VALUES="$YQ_VALUES |
-    .externalDatabase.host = \"$PG_HOST\" |
-    .externalDatabase.port = \"5432\" |
-    .externalDatabase.user = \"$PG_USER\" |
-    .externalDatabase.password = \"$PG_PASSWORD\" |
-    .externalDatabase.database = \"$PG_DATABASE_NAME\"
-  "
 }
--- vkpr/keycloak/remove/src/unix/formula/formula.sh
+++ vkpr/keycloak/remove/src/unix/formula/formula.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 runFormula() {
-  bold "$(info "Removing Keycloak...")"
+  info "Removing Keycloak..."
 
-  KEYCLOAK_NAMESPACE=$($VKPR_KUBECTL get po -A -l app.kubernetes.io/instance=keycloak,vkpr=true -o=yaml | $VKPR_YQ e ".items[].metadata.namespace" - | head -n1)
-  $VKPR_HELM uninstall keycloak -n "$KEYCLOAK_NAMESPACE"
+  HELM_FLAG="-A"
+  [[ "$VKPR_ENVIRONMENT" == "okteto" ]] && HELM_FLAG=""
+  KEYCLOAK_NAMESPACE=$($VKPR_HELM ls -o=json $HELM_FLAG |\
+                     $VKPR_JQ -r '.[] | select(.name | contains("keycloak")) | .namespace' |\
+                     head -n1)
+
+  $VKPR_HELM uninstall keycloak -n "$KEYCLOAK_NAMESPACE" 2> /dev/null || error "VKPR Keycloak not found"
 }
