37ffe9c576a4b96c7dda0887c1f7c994895b41d4
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-06 14:54:10 -0300

    fix(external-dns): update formula structure

--- vkpr/external-dns/install/aws/src/unix/formula/formula.sh
+++ vkpr/external-dns/install/aws/src/unix/formula/formula.sh
@@ -1,61 +1,46 @@
 #!/bin/bash
 
 runFormula() {
-  # Global values
-  checkGlobalConfig "$VKPR_K8S_NAMESPACE" "vkpr" "global.namespace" "GLOBAL_NAMESPACE"
+  local VKPR_EXTERNAL_DNS_VALUES YQ_VALUES HELM_ARGS;
+  setCredentials
+  formulaInputs
+  validateInputs
 
-  # App values
-  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "external-dns.namespace" "EXTERNAL_DNS_NAMESPACE"
-  checkGlobalConfig "false" "false" "external-dns.metrics" "EXTERNAL_DNS_METRICS"
-
-  local VKPR_EXTERNAL_DNS_VALUES; VKPR_EXTERNAL_DNS_VALUES="$(dirname "$0")"/utils/external-dns.yaml
+  VKPR_EXTERNAL_DNS_VALUES="$(dirname "$0")"/utils/external-dns.yaml
 
   startInfos
-  addRepoExternalDNS
-  installExternalDNS
+  settingExternalDNS
+  [[ $DRY_RUN == false ]] && registerHelmRepository bitnami https://charts.bitnami.com/bitnami
+  installApplication "external-dns" "bitnami/external-dns" "$VKPR_ENV_EXTERNAL_DNS_NAMESPACE" "$VKPR_EXTERNAL_DNS_VERSION" "$VKPR_EXTERNAL_DNS_VALUES" "$HELM_ARGS"
 }
 
 startInfos() {
-  echo "=============================="
-  bold "$(info "VKPR External-DNS Install AWS Routine")"
-  echo "=============================="
+  bold "=============================="
+  boldNotice "$(info "VKPR External-DNS Install AWS Routine")"
+  bold "=============================="
 }
 
-addRepoExternalDNS() {
-  registerHelmRepository bitnami https://charts.bitnami.com/bitnami
+formulaInputs() {
+  # App values
+  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "external-dns.namespace" "EXTERNAL_DNS_NAMESPACE"
+  checkGlobalConfig "false" "false" "external-dns.metrics" "EXTERNAL_DNS_METRICS"
 }
 
-installExternalDNS() {
-  bold "$(info "Installing External-DNS AWS...")"
-  local YQ_VALUES=".rbac.create = true"
-  settingExternalDNS
-
-  if [[ $DRY_RUN == true ]]; then
-    bold "---"
-    mergeVkprValuesHelmArgs "external-dns" "$VKPR_EXTERNAL_DNS_VALUES"
-    $VKPR_YQ eval "$YQ_VALUES" "$VKPR_EXTERNAL_DNS_VALUES"    
-  else
-    bold "$(info "Installing External-DNS AWS...")"
-    $VKPR_YQ eval -i "$YQ_VALUES" "$VKPR_EXTERNAL_DNS_VALUES"
-    mergeVkprValuesHelmArgs "external-dns" "$VKPR_EXTERNAL_DNS_VALUES"
-    $VKPR_HELM upgrade -i --version "$VKPR_EXTERNAL_DNS_VERSION" \
-      --namespace "$VKPR_ENV_EXTERNAL_DNS_NAMESPACE" --create-namespace \
-      --wait -f "$VKPR_EXTERNAL_DNS_VALUES" external-dns bitnami/external-dns
-  fi
+setCredentials() {
+  AWS_REGION=$($VKPR_JQ -r .credential.region "$VKPR_CREDENTIAL"/aws)
+  AWS_ACCESS_KEY=$($VKPR_JQ -r .credential.accesskeyid "$VKPR_CREDENTIAL"/aws)
+  AWS_SECRET_KEY=$($VKPR_JQ -r .credential.secretaccesskey "$VKPR_CREDENTIAL"/aws)
 }
 
-
-settingExternalDNS() {
-  AWS_REGION=$($VKPR_JQ -r .credential.region ~/.rit/credentials/default/aws)
-  AWS_ACCESS_KEY=$($VKPR_JQ -r .credential.accesskeyid ~/.rit/credentials/default/aws)
-  AWS_SECRET_KEY=$($VKPR_JQ -r .credential.secretaccesskey ~/.rit/credentials/default/aws)
-
+validateInputs() {
   validateAwsAccessKey "$AWS_ACCESS_KEY"
   validateAwsSecretKey "$AWS_SECRET_KEY"
   validateAwsRegion "$AWS_REGION"
+}
 
-  YQ_VALUES="$YQ_VALUES |
-    .provider = \"aws\" |
+settingExternalDNS() {
+  YQ_VALUES=".provider = \"aws\" |
+    .rbac.create = true |
     .aws.credentials.accessKey = \"$AWS_ACCESS_KEY\" |
     .aws.credentials.secretKey = \"$AWS_SECRET_KEY\" |
     .aws.region = \"$AWS_REGION\"
@@ -63,10 +48,20 @@ settingExternalDNS() {
 
   if [[ $VKPR_ENV_EXTERNAL_DNS_METRICS == true ]]; then
     YQ_VALUES="$YQ_VALUES |
-      .metrics.enabled = \"true\" |
-      .metrics.serviceMonitor.enabled = \"true\" |
+      .metrics.enabled = true |
+      .metrics.serviceMonitor.enabled = true |
       .metrics.serviceMonitor.namespace = \"$VKPR_ENV_INGRESS_NAMESPACE\" |
       .metrics.serviceMonitor.interval = \"1m\"
     "
+  fi
+  settingExternaldnsEnvironment
+
+  debug "YQ_CONTENT = $YQ_VALUES"
+}
+
+settingExternaldnsEnvironment() {
+  if [[ "$VKPR_ENVIRONMENT" == "okteto" ]]; then
+    HELM_ARGS="--cleanup-on-fail"
+    YQ_VALUES="$YQ_VALUES"
   fi
 }
--- vkpr/external-dns/install/digitalocean/src/unix/formula/formula.sh
+++ vkpr/external-dns/install/digitalocean/src/unix/formula/formula.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 runFormula() {
-  # App values
-  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "external-dns.namespace" "EXTERNAL_DNS_NAMESPACE"
-  checkGlobalConfig "false" "false" "external-dns.metrics" "EXTERNAL_DNS_METRICS"
+  local VKPR_EXTERNAL_DNS_VALUES YQ_VALUES HELM_ARGS;
+  setCredentials
+  formulaInputs
+  validateInputs
 
-  local VKPR_EXTERNAL_DNS_VALUES; VKPR_EXTERNAL_DNS_VALUES="$(dirname "$0")"/utils/external-dns.yaml
+  VKPR_EXTERNAL_DNS_VALUES="$(dirname "$0")"/utils/external-dns.yaml
 
   startInfos
-  addRepoExternalDNS
-  installExternalDNS
+  settingExternalDNS
+  [[ $DRY_RUN == false ]] && registerHelmRepository bitnami https://charts.bitnami.com/bitnami
+  installApplication "external-dns" "bitnami/external-dns" "$VKPR_ENV_EXTERNAL_DNS_NAMESPACE" "$VKPR_EXTERNAL_DNS_VERSION" "$VKPR_EXTERNAL_DNS_VALUES" "$HELM_ARGS"
 }
 
 startInfos() {
@@ -18,45 +20,42 @@ startInfos() {
   echo "=============================="
 }
 
-addRepoExternalDNS() {
-  registerHelmRepository bitnami https://charts.bitnami.com/bitnami
+formulaInputs() {
+  # App values
+  checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "external-dns.namespace" "EXTERNAL_DNS_NAMESPACE"
+  checkGlobalConfig "false" "false" "external-dns.metrics" "EXTERNAL_DNS_METRICS"
 }
 
-installExternalDNS() {
-  bold "$(info "Installing External-DNS Digitalocean...")"
-  local YQ_VALUES=".rbac.create = true"
-  settingExternalDNS
-
-  if [[ $DRY_RUN == true ]]; then
-    bold "---"
-    mergeVkprValuesHelmArgs "external-dns" "$VKPR_EXTERNAL_DNS_VALUES"
-    $VKPR_YQ eval "$YQ_VALUES" "$VKPR_EXTERNAL_DNS_VALUES"    
-  else
-    bold "$(info "Installing External-DNS Digitalocean...")"
-    $VKPR_YQ eval -i "$YQ_VALUES" "$VKPR_EXTERNAL_DNS_VALUES"
-    mergeVkprValuesHelmArgs "external-dns" "$VKPR_EXTERNAL_DNS_VALUES"
-    $VKPR_HELM upgrade -i --version "$VKPR_EXTERNAL_DNS_VERSION" \
-      --namespace "$VKPR_ENV_EXTERNAL_DNS_NAMESPACE" --create-namespace \
-      --wait -f "$VKPR_EXTERNAL_DNS_VALUES" external-dns bitnami/external-dns
-  fi
+setCredentials() {
+  DO_TOKEN=$($VKPR_JQ -r .credential.token "$VKPR_CREDENTIAL"/digitalocean)
 }
 
+validateInputs() {
+  validateDigitalOceanApiToken "$DO_TOKEN"
+}
 
 settingExternalDNS() {
-  DO_TOKEN=$($VKPR_JQ -r .credential.token ~/.rit/credentials/default/digitalocean)
-  validateDigitalOceanApiToken "$DO_TOKEN"
-
-  YQ_VALUES="$YQ_VALUES |
-    .provider = \"digitalocean\" |
+  YQ_VALUES=".provider = \"digitalocean\" |
+    .rbac.create = true |
     .digitalocean.apiToken = \"$DO_TOKEN\"
   "
 
   if [[ $VKPR_ENV_EXTERNAL_DNS_METRICS == true ]]; then
     YQ_VALUES="$YQ_VALUES |
-      .metrics.enabled = \"true\" |
-      .metrics.serviceMonitor.enabled = \"true\" |
+      .metrics.enabled = true |
+      .metrics.serviceMonitor.enabled = true |
       .metrics.serviceMonitor.namespace = \"$VKPR_ENV_INGRESS_NAMESPACE\" |
       .metrics.serviceMonitor.interval = \"1m\"
     "
+  fi
+  settingExternaldnsEnvironment
+
+  debug "YQ_CONTENT = $YQ_VALUES"
+}
+
+settingExternaldnsEnvironment() {
+  if [[ "$VKPR_ENVIRONMENT" == "okteto" ]]; then
+    HELM_ARGS="--cleanup-on-fail"
+    YQ_VALUES="$YQ_VALUES"
   fi
 }
