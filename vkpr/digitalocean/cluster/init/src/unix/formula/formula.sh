6a474e14c19ec49690ef20f2cd38397d6dbded3e
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-06 14:30:39 -0300

    fix(digitalocean): update formula structure

--- vkpr/digitalocean/cluster/init/src/unix/formula/formula.sh
+++ vkpr/digitalocean/cluster/init/src/unix/formula/formula.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
-PROJECT_ENCODED=$(rawUrlEncode "${GITLAB_USERNAME}/k8s-digitalocean")
-
 runFormula() {
+  local DO_CLUSTER_NODE_INSTANCE_TYPE PROJECT_ENCODED FORK_RESPONSE_CODE;
+
   #getting real instance type
   DO_CLUSTER_NODE_INSTANCE_TYPE=${DO_CLUSTER_NODE_INSTANCE_TYPE// ([^)]*)/}
   DO_CLUSTER_NODE_INSTANCE_TYPE=${DO_CLUSTER_NODE_INSTANCE_TYPE// /}
+  
+  formulaInputs
+  setCredentials
+  validateInputs
+  
+  PROJECT_ENCODED=$(rawUrlEncode "${GITLAB_USERNAME}/aws-eks")
+  FORK_RESPONSE_CODE=$(curl -siX POST -H "PRIVATE-TOKEN: ${GITLAB_TOKEN}" \
+    "https://gitlab.com/api/v4/projects/$(rawUrlEncode "vkpr/aws-eks")/fork" |\
+    head -n 1 | awk -F' ' '{print $2}'
+  )
 
+  debug "FORK_RESPONSE_CODE=$FORK_RESPONSE_CODE"
+  if [ "$FORK_RESPONSE_CODE" == "409" ];then
+    boldNotice "Project already forked"
+  fi
+  
+  setVariablesGLAB
+  cloneRepository
+}
+
+formulaInputs() {
+  # App values
   checkGlobalConfig "$DO_CLUSTER_NAME" "do-sample" "digitalocean.cluster.name" "DO_CLUSTER_NAME"
   checkGlobalConfig "$DO_K8S_VERSION" "1.22" "digitalocean.cluster.version" "DO_CLUSTER_VERSION"
   checkGlobalConfig "$DO_CLUSTER_REGION" "nyc1" "digitalocean.cluster.region" "DO_CLUSTER_REGION"
   checkGlobalConfig "$DO_CLUSTER_NODE_INSTANCE_TYPE" "s-2vcpu-2gb" "digitalocean.cluster.nodes.instaceType" "DO_CLUSTER_NODES_INSTANCE_TYPE"
   checkGlobalConfig "$DO_CLUSTER_SIZE" "1" "digitalocean.cluster.nodes.quantitySize" "DO_CLUSTER_QUANTITY_SIZE"
+}
 
+setCredentials() {
+  DO_TOKEN="$($VKPR_JQ -r '.credential.token' $VKPR_CREDENTIAL/digitalocean)"
+  GITLAB_USERNAME="$($VKPR_JQ -r '.credential.username' $VKPR_CREDENTIAL/gitlab)"
+  GITLAB_TOKEN="$($VKPR_JQ -r '.credential.token' $VKPR_CREDENTIAL/gitlab)"
+}
+
+validateInputs() {
   validateDigitalOceanApiToken "$DO_TOKEN"
   validateGitlabUsername "$GITLAB_USERNAME"
   validateGitlabToken "$GITLAB_TOKEN"
-
-
-  local FORK_RESPONSE_CODE
-  FORK_RESPONSE_CODE=$(curl -siX POST -H "PRIVATE-TOKEN: ${GITLAB_TOKEN}" \
-    "https://gitlab.com/api/v4/projects/$(rawUrlEncode "vkpr/k8s-digitalocean")/fork" |\
-    head -n 1 | awk -F' ' '{print $2}' 2> /dev/null
-  )
-  #echo "FORK_RESPONSE_CODE= $FORK_RESPONSE_CODE"
-  if [ "$FORK_RESPONSE_CODE" == "409" ];then
-    warn "Project already forked"
-  fi
-  local VKPR_DIGITALOCEAN_VALUES; VKPR_DIGITALOCEAN_VALUES="$(dirname "$0")"/utils/digitalocean.yaml
-
-  setVariablesGLAB
-  cloneRepository
 }
 
-## Set all input into Gitlab environments
 setVariablesGLAB() {
   createOrUpdateVariable "$PROJECT_ENCODED" "DO_TOKEN" "$DO_TOKEN" "yes" "$VKPR_ENV_DO_CLUSTER_NAME" "$GITLAB_TOKEN"
 }
