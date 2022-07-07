53d6bef69e1e217cd8e484ee766a8b95317d2bf2
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-07 14:04:55 -0300

    fix(aws/rds): update formula structure

--- vkpr/aws/rds/destroy/src/unix/formula/formula.sh
+++ vkpr/aws/rds/destroy/src/unix/formula/formula.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 runFormula() {
+  setCredentials
+  validateInputs
+
+  info "Destroying db instance..."
   aws rds delete-db-instance \
     --db-instance-identifier "$RDS_INSTANCE_NAME" \
-    --skip-final-snapshot 1> /dev/null && echo "Database destroyed"
+    --skip-final-snapshot 1> /dev/null && boldNotice "Database destroyed"
+}
+
+setCredentials() {
+  AWS_ACCESS_KEY="$($VKPR_JQ -r '.credential.accesskeyid' $VKPR_CREDENTIAL/aws)"
+  AWS_SECRET_KEY="$($VKPR_JQ -r '.credential.secretaccesskey' $VKPR_CREDENTIAL/aws)"
+  AWS_REGION="$($VKPR_JQ -r '.credential.region' $VKPR_CREDENTIAL/aws)"
+}
+
+validateInputs() {
+  validateAwsSecretKey "$AWS_SECRET_KEY"
+  validateAwsAccessKey "$AWS_ACCESS_KEY"
+  validateAwsRegion "$AWS_REGION"
 }
--- vkpr/aws/rds/init/src/unix/formula/formula.sh
+++ vkpr/aws/rds/init/src/unix/formula/formula.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 runFormula() {
+  setCredentials
+  validateInputs
 
-  validateAwsSecretKey "$AWS_SECRET_KEY"
-  validateAwsAccessKey "$AWS_ACCESS_KEY"
-  validateAwsRegion "$AWS_REGION"
-
+  info "Destroying db instance..."
   aws rds create-db-instance \
     --db-instance-identifier "$RDS_INSTANCE_NAME" \
     --db-instance-class "$RDS_INSTANCE_TYPE" \
     --engine postgres \
     --master-username "$DBUSER" \
     --master-user-password "$DBPASSWORD" \
-    --allocated-storage 20 1> /dev/null && echo "Database created"
+    --allocated-storage 20 1> /dev/null && boldNotice "Database created"
     
+}
+
+setCredentials() {
+  AWS_ACCESS_KEY="$($VKPR_JQ -r '.credential.accesskeyid' $VKPR_CREDENTIAL/aws)"
+  AWS_SECRET_KEY="$($VKPR_JQ -r '.credential.secretaccesskey' $VKPR_CREDENTIAL/aws)"
+  AWS_REGION="$($VKPR_JQ -r '.credential.region' $VKPR_CREDENTIAL/aws)"
+}
+
+validateInputs() {
+  validateAwsSecretKey "$AWS_SECRET_KEY"
+  validateAwsAccessKey "$AWS_ACCESS_KEY"
+  validateAwsRegion "$AWS_REGION"
 }
