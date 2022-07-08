3f225d3e3460d7fb2b0bbce523ef6ba903965ee2
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-07-08 18:22:51 -0300

    feat(import): import loki logs dashboard

--- vkpr/prometheus-stack/import/src/unix/formula/formula.sh
+++ vkpr/prometheus-stack/import/src/unix/formula/formula.sh
@@ -3,9 +3,6 @@
 runFormula() {
   checkGlobalConfig "$VKPR_ENV_GLOBAL_NAMESPACE" "$VKPR_ENV_GLOBAL_NAMESPACE" "prometheus-stack.namespace" "PROMETHEUS_STACK_NAMESPACE"
 
-  info "Importing Dashboard..."
-  dashboard="$DASHBOARD_NAME-grafana" \
-  $VKPR_YQ eval "(.metadata.name, .metadata.labels.app) = strenv(dashboard) |
-    .data.[\"$DASHBOARD_NAME.json\"] = ($(cat $DASHBOARD_PATH) | to_json)
-  " "$(dirname "$0")"/utils/dashboard.yaml | $VKPR_KUBECTL apply -n $VKPR_ENV_PROMETHEUS_STACK_NAMESPACE -f -
+  info "Importing dashboard..."
+  createGrafanaDashboard "$DASHBOARD_NAME" "$DASHBOARD_PATH" "$VKPR_ENV_PROMETHEUS_STACK_NAMESPACE"
 }
