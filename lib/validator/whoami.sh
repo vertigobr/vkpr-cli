3b64c630c23a34a8d2b7ad9abd0178fb8bcc1cad
Sandro Flausino <sjflausino@gmail.com>
2022-07-27 11:21:49 -0300

    feat(whoami) adding input validators

--- lib/validator/whoami.sh
+++ lib/validator/whoami.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# -----------------------------------------------------------------------------
+# Whoami validators
+# -----------------------------------------------------------------------------
+
 validateWhoamiSecure(){
+  echo "$1"
   if $(validateBool $1); then
     return 
   else
@@ -8,3 +13,54 @@ validateWhoamiSecure(){
     exit
   fi
 }
+
+validateWhoamiDomain (){
+  echo "$1"
+  if  $(validateDomain $1); then
+    return
+  else
+    error "Please correctly enter the domain to be used "
+    exit
+  fi
+}
+
+validateWhoamiSecure (){
+  echo "$1"
+  if $(validateBool $1); then
+    return
+  else
+    error "It was not possible to identify if the application will use HTTPS"
+    exit
+  fi
+}
+
+validateWhoamiSsl (){
+  echo "$1"
+  if $(validateBool $1); then
+    return
+  else
+    error "It was not possible to identify if the application will use SSL"
+    exit
+  fi
+}
+
+validateWhoamiSslCrtPath (){
+  echo "$1"
+  if $(validatePath $1); then
+    return
+  else
+    error "Invalid path for SSL .crt file"
+    exit
+  fi
+}
+
+validateWhoamiSslKeyPath (){
+  echo "$1"
+  if $(validatePath $1); then
+    return
+  else
+    error "Invalid path for SSL .key file"
+    exit
+  fi
+}
+
