2a928b3bbdba605bc81d190ce84b10bf8527cd48
João Pedro Pedroza <pedrozajoaopedro@gmail.com>
2022-08-08 18:36:22 -0300

    fix(gitlab): reajust message error

--- lib/validator/gitlab.sh
+++ lib/validator/gitlab.sh
@@ -8,7 +8,7 @@ validateGitlabToken() {
   if [[ "$1" =~ ^([A-Za-z0-9-]{26})$ ]]; then
     return
     else
-    error "The value used for GITLAB_TOKEN "$GITLAB_TOKEN" is invalid: GITLAB_TOKEN must consist of lowercase, uppercase or '-' alphanumeric characters, (e.g. 'Aa1Bb2Cc3Dd4Ee5Ff6Gg7Hh8Ii', regex used for validation is ^([A-Za-z0-9-]{26})$."
+    error "The value used for GITLAB_TOKEN \"$GITLAB_TOKEN\" is invalid: GITLAB_TOKEN must consist of lowercase, uppercase or '-' alphanumeric characters, (e.g. 'Aa1Bb2Cc3Dd4Ee5Ff6Gg7Hh8Ii', regex used for validation is ^([A-Za-z0-9-]{26})$."
     exit
   fi
 }
@@ -17,7 +17,7 @@ validateGitlabUsername() {
   if [[ "$1" =~ ^([A-Za-z0-9-]+)$ ]]; then
     return
     else
-    error "The value used for GITLAB_USERNAME "$GITLAB_USERNAME" is invalid: GITLAB_USERNAME must consist of lowercase, uppercase or '-' alphanumeric characters, (e.g. 'gitlab', regex used for validation is ^([A-Za-z0-9-]+)$."
+    error "The value used for GITLAB_USERNAME \"$GITLAB_USERNAME\" is invalid: GITLAB_USERNAME must consist of lowercase, uppercase or '-' alphanumeric characters, (e.g. 'gitlab', regex used for validation is ^([A-Za-z0-9-]+)$."
     exit
   fi
 }
