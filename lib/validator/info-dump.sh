6d72060944576c73aedceafe1b185a5b650f4179
Sandro Flausino <sjflausino@gmail.com>
2022-09-20 17:49:44 -0300

    feat(info-dump) fixing syntax error in declaration adding okteto compatibility

--- lib/validator/info-dump.sh
+++ lib/validator/info-dump.sh
@@ -9,25 +9,16 @@ validateInfoDumpPath (){
     if $(validatePath $1); then
         return
     else
-        error "The value used for APLICATION_PATH \"$1\" is invalid: APLICATION_PATH must consist of lowercase and '-' alphanumeric characters, (e.g. '/tmp/certificate.key', regex used for validation is ^(\/[^\/]+){1,}\/?$')"
+        error "The value used for APPLICATION_PATH \"$1\" is invalid: APPLICATION_PATH must consist of lowercase, uppercase and '-' alphanumeric characters, (e.g. '/home/user', regex used for validation is ^(\/[^\/]+){1,}\/?$')"
         exit
     fi
-}
-
-validateInfoDumpNamespace(){
-  if $(validateNamespace "$1"); then
-    return
-  else
-    error "The value used for APLICATION_NAMESPACE \"$1\" is invalid: APLICATION_NAMESPACE must consist of lowercase and '-' alphanumeric characters, (e.g. 'ingress', regex used for validation is ^([A-Za-z0-9-]+)$')"
-    exit
-  fi
 }
 
 validateInfoDumpName(){
   if $(validateNamespace "$1"); then
     return
   else
-    error "The value used for APLICATION_NAME \"$1\" is invalid: APLICATION_NAME must consist of lowercase and '-' alphanumeric characters, (e.g. 'ingress', regex used for validation is ^([A-Za-z0-9-]+)$')"
+    error "The value used for APPLICATION_NAME \"$1\" is invalid: APPLICATION_NAME must consist of lowercase and '-' alphanumeric characters, (e.g. 'ingress', regex used for validation is ^([A-Za-z0-9-]+)$')"
     exit
   fi
 }
--- vkpr/info/dump/src/unix/formula/formula.sh
+++ vkpr/info/dump/src/unix/formula/formula.sh
@@ -2,72 +2,78 @@
 
 runFormula() {
 
-  [[ "$APLICATION_PATH" == "" ]] && APLICATION_PATH=$CURRENT_PWD
+  [[ "$APPLICATION_PATH" == "" ]] && APPLICATION_PATH=$CURRENT_PWD
   validateInputs
 
-  startInfos
-
-  local DIR_NAME="vkpr-logs-$APLICATION_NAME-$(date -I)" 
+  local DIR_NAME="vkpr-logs-$APPLICATION_NAME-$(date -I)" 
   
+  HELM_FLAG="-A"
+  [[ "$VKPR_ENVIRONMENT" == "okteto" ]] && HELM_FLAG=""
+  APPLICATION_NAMESPACE=$($VKPR_HELM ls -o=json $HELM_FLAG |\
+                     $VKPR_JQ -r ".[] | select(.name | contains(\"$APPLICATION_NAME\")) | .namespace" |\
+                     head -n1)
+                        
   # Making directories to add logs..
-  mkdir -p "$APLICATION_PATH/$DIR_NAME"
+  startInfos
+  checkApplication
+  mkdir -p "$APPLICATION_PATH/$DIR_NAME"
+
 
   # Extracting logs..
-  checkAplication
   listPods
   logPods
-  describeAplication
+  describeApplication
   describeNodes
-  valuesAplication
+  valuesApplication
 
   # Compressing Logs..
-  zip -r $DIR_NAME.zip "$APLICATION_PATH/$DIR_NAME"/*
-  rm -r "$APLICATION_PATH/$DIR_NAME"
+  cd $APPLICATION_PATH
+  zip -r $DIR_NAME.zip $DIR_NAME/*
+  rm -r "$APPLICATION_PATH/$DIR_NAME"
 }
 
 startInfos() {
   bold "=============================="
   boldInfo "VKPR Info Dump Routine"
-  boldNotice "Aplication Name: $APLICATION_NAME"
-  boldNotice "Aplication Path: $APLICATION_PATH"
-  boldNotice "Aplication Namespace: $APLICATION_NAMESPACE"
+  boldNotice "Application Name: $APPLICATION_NAME"
+  boldNotice "Application Path: $APPLICATION_PATH"
+  boldNotice "Application Namespace: $APPLICATION_NAMESPACE"
   bold "=============================="
 }
 
 validateInputs() {
   # Local App Values
-  validateInfoDumpPath "$APLICATION_PATH"
-  validateInfoDumpNamespace "$APLICATION_NAMESPACE"
-  validateInfoDumpName "$APLICATION_NAME"
+  validateInfoDumpPath "$APPLICATION_PATH"
+  validateInfoDumpName "$APPLICATION_NAME"
 }
 
-checkAplication(){
-  if $($VKPR_HELM list -n $APLICATION_NAMESPACE | awk 'NR>1{print $1}'| grep -q $APLICATION_NAME) ; then
+checkApplication(){
+  if $($VKPR_HELM list -n $APPLICATION_NAMESPACE | awk 'NR>1{print $1}'| grep -q "$APPLICATION_NAME") ; then
     boldInfo "Starting log extraction process..."
   else
-    error "Application not found or not specified correctly, please check namespace and application name. "
+    error "Application not found or not specified correctly, please check the application name."
     exit
   fi
 }
 
 logPods(){
   declare -i COUNT=0
-  for pod in $($VKPR_KUBECTL get pods -l app.kubernetes.io/instance=$APLICATION_NAME,app.kubernetes.io/managed-by=vkpr -A --ignore-not-found  | awk 'NR>1{print $2}'); do
-    $VKPR_KUBECTL logs $pod -n $APLICATION_NAMESPACE --all-containers | tac > $APLICATION_PATH/$DIR_NAME/vkpr-logs-pod-$pod-$COUNT.txt
+  for pod in $($VKPR_KUBECTL get pods -l app.kubernetes.io/instance=$APPLICATION_NAME,app.kubernetes.io/managed-by=vkpr -A --ignore-not-found  | awk 'NR>1{print $2}'); do
+    $VKPR_KUBECTL logs $pod -n $APPLICATION_NAMESPACE --all-containers | tac > $APPLICATION_PATH/$DIR_NAME/vkpr-logs-pod-$pod-$COUNT.txt
     ((COUNT++))
   done
 }
 
 listPods(){
-  $VKPR_KUBECTL get pods -l app.kubernetes.io/instance=$APLICATION_NAME,app.kubernetes.io/managed-by=vkpr -A --ignore-not-found > $APLICATION_PATH/$DIR_NAME/"vkpr-list-pods".txt
+  $VKPR_KUBECTL get pods -l app.kubernetes.io/instance=$APPLICATION_NAME,app.kubernetes.io/managed-by=vkpr $HELM_FLAG --ignore-not-found > $APPLICATION_PATH/$DIR_NAME/"vkpr-list-pods".txt
 }
 
-describeAplication(){
+describeApplication(){
   declare -i COUNT=0
   for type in pod svc ingress; do
-    for resourse in $($VKPR_KUBECTL get $type -n $APLICATION_NAMESPACE --ignore-not-found  | awk 'NR>1{print $1}'); do
-      if [[ "$resourse" == *"$APLICATION_NAME"* ]]; then
-        $VKPR_KUBECTL describe "$type/$resourse" -n $APLICATION_NAMESPACE > $APLICATION_PATH/$DIR_NAME/"vkpr-describe-$type-$resourse-$COUNT".txt
+    for resourse in $($VKPR_KUBECTL get $type -n $APPLICATION_NAMESPACE --ignore-not-found  | awk 'NR>1{print $1}'); do
+      if [[ "$resourse" == *"$APPLICATION_NAME"* ]]; then
+        $VKPR_KUBECTL describe "$type/$resourse" -n $APPLICATION_NAMESPACE > $APPLICATION_PATH/$DIR_NAME/"vkpr-describe-$type-$resourse-$COUNT".txt
         ((COUNT++))
       fi
     done
@@ -77,13 +83,13 @@ describeAplication(){
 
 describeNodes(){
   declare -i COUNT=0
-  for node in $($VKPR_KUBECTL get node -n $APLICATION_NAMESPACE --ignore-not-found  | awk 'NR>1{print $1}'); do
-    $VKPR_KUBECTL describe "node/$node" -n $APLICATION_NAMESPACE > $APLICATION_PATH/$DIR_NAME/"vkpr-describe-node-$node-$COUNT".txt
+  for node in $($VKPR_KUBECTL get node -n $APPLICATION_NAMESPACE --ignore-not-found  | awk 'NR>1{print $1}'); do
+    $VKPR_KUBECTL describe "node/$node" -n $APPLICATION_NAMESPACE > $APPLICATION_PATH/$DIR_NAME/"vkpr-describe-node-$node-$COUNT".txt
     ((COUNT++))
   done
   COUNT=0
 }
 
-valuesAplication(){        
-  $VKPR_HELM get values $APLICATION_NAME -n $APLICATION_NAMESPACE > $APLICATION_PATH/$DIR_NAME/"vkpr-values-$APLICATION_NAME".txt
+valuesApplication(){        
+  $VKPR_HELM get values $APPLICATION_NAME -n $APPLICATION_NAMESPACE > $APPLICATION_PATH/$DIR_NAME/"vkpr-values-$APPLICATION_NAME".txt
 }
