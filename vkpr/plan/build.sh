ce1bd3d82b42b8ef093bb01fb6e3a40495b697d3
Sandro Flausino <sjunior@vertigo.com.br>
2023-06-14 15:45:24 -0300

    feat (vkpr plan) creating plan formula and adding helm diff in install function

---
+++ vkpr/plan/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+BIN_FOLDER=bin
+BINARY_NAME_UNIX=run.sh
+ENTRY_POINT_UNIX=main.sh
+LIB_RESOURCES="../../lib"
+
+#bash-build:
+	mkdir -p $BIN_FOLDER/src
+	#shellcheck disable=SC2086
+  cp -r $LIB_RESOURCES $BIN_FOLDER/src
+	cp -r src/* $BIN_FOLDER
+	mv $BIN_FOLDER/$ENTRY_POINT_UNIX $BIN_FOLDER/$BINARY_NAME_UNIX
+	chmod +x $BIN_FOLDER/$BINARY_NAME_UNIX
