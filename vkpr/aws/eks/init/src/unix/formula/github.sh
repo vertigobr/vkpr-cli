f7dd81798bc2d9387ff20c6eb51b890cbf1b7971
Sandro Flausino <sjunior@vertigo.com.br>
2023-07-19 13:40:33 -0300

    feat(eks) updating kubernetes versions in list

--- vkpr/aws/eks/init/src/unix/formula/github.sh
+++ vkpr/aws/eks/init/src/unix/formula/github.sh
@@ -5,7 +5,7 @@ setproviderrun() {
 formulaInputs() {
   # App values
   checkGlobalConfig "$CLUSTER_NAME" "eks-sample" "aws.eks.clusterName" "EKS_CLUSTER_NAME"
-  checkGlobalConfig "$K8S_VERSION" "1.23" "aws.eks.version" "EKS_VERSION"
+  checkGlobalConfig "$K8S_VERSION" "1.25" "aws.eks.version" "EKS_VERSION"
   checkGlobalConfig "$CLUSTER_NODE_INSTANCE_TYPE.$CLUSTER_NODE_INSTANCE_SIZE" "t3.small" "aws.eks.nodes.instanceType" "EKS_NODES_INSTANCE_TYPE"
   checkGlobalConfig "$CLUSTER_SIZE" "1" "aws.eks.nodes.quantitySize" "EKS_NODES_QUANTITY_SIZE"
   checkGlobalConfig "$CAPACITY_TYPE" "on_demand" "aws.eks.nodes.capacityType" "EKS_NODES_CAPACITY_TYPE"
