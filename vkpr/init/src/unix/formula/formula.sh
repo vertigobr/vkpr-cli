71b3cc972d20d8d5eef5026b191abff8dcd33721
Sandro Flausino <sjunior@vertigo.com.br>
2023-08-11 15:09:40 -0300

    fix(init) adding check sudo for docker usage

--- vkpr/init/src/unix/formula/formula.sh
+++ vkpr/init/src/unix/formula/formula.sh
@@ -33,7 +33,11 @@ runFormula() {
 
 installeksctl(){
  curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
- sudo mv /tmp/eksctl /usr/local/bin
+  if command -v sudo &> /dev/null; then
+    sudo mv /tmp/eksctl /usr/local/bin
+  else
+    mv /tmp/eksctl /usr/local/bin
+  fi
 }
 
 installArkade() {
